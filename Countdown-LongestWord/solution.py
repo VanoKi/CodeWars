2091c8e95ec80bc03f1f0fa0e8582026b51f5dc2
VanoKi <kirsvano@gmail.com>
2024-10-22 20:42:30 +0300

    feat: create a function

--- Countdown-LongestWord/solution.py
+++ Countdown-LongestWord/solution.py
@@ -1,3 +1,15 @@
 from icecream import ic
-import words
-ic(len(words))
+from words import words
+
+letters = 'GQEMAUVXY'
+def longest_word(letters):
+    word2 = ''
+    ans = []
+    for word in words:
+        for char in word:
+            if char in letters:
+                word2 += char
+        ans.append(word2)
+    return ans
+
+ic(longest_word(letters))
