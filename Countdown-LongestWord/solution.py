7e75d5dbbdb49eb602e4174abee760c101f91b7e
VanoKi <kirsvano@gmail.com>
2024-10-23 17:54:15 +0300

    feat: add a third function for sort result

--- Countdown-LongestWord/solution.py
+++ Countdown-LongestWord/solution.py
@@ -1,7 +1,7 @@
 from icecream import ic
 from words import words
 
-letters = 'GQEMAUVXY'
+letters = 'TDWAYZROE'
 
 def check_word(letters, word):
     letters_len = len(letters)
@@ -15,17 +15,59 @@ def longest_word(letters):
     for word in words:
         result_of_compare = check_word(letters, word)
         if result_of_compare:
-            # ans.append(result_of_compare)
-            if ans:
-                if len(result_of_compare) > len(ans[0]):
-                    if ans[0] < result_of_compare:
-                        ans[0] = result_of_compare
-                    else:
-                        ans.append(result_of_compare)
-            else:
+            if not ans:
+                ans.append(result_of_compare)
+            elif len(result_of_compare) > len(ans[0]):
+                ans = [result_of_compare]
+            elif len(result_of_compare) == len(ans[0]):
                 ans.append(result_of_compare)
     return ans
 
 # longest_word(letters)
 ic(longest_word(letters))
 # ic(check_word(letters, "GAME"))
+
+def check_result():
+    words = ['ART',
+                            'DARE',
+                            'DART',
+                            'DATE',
+                            'DAY',
+                            'DEW',
+                            'DOT',
+                            'DYE',
+                            'EAR',
+                            'OAR',
+                            'ORE',
+                            'RATE',
+                            'RAY',
+                            'ROAD',
+                            'ROD',
+                            'TAR',
+                            'TEAR',
+                            'TODAY',
+                            'TOE',
+                            'TOWER',
+                            'TRADE',
+                            'TRAY',
+                            'TRY',
+                            'WAR',
+                            'WATER',
+                            'WAY',
+                            'WEAR',
+                            'WORD',
+                            'YARD',
+                            'YAW',
+                            'YEAR',
+                            'ZERO']
+    ans = []
+    for word in words:
+        if not ans:
+            ans.append(word)
+        elif len(word) > len(ans[0]):
+            ans = [word]
+        elif len(word) == len(ans[0]):
+            ans.append(word)
+    return ans
+
+# ic(check_result())
