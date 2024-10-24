a7624fb6262120dd4e5fb45ada4b283ef03caf41
VanoKi <kirsvano@gmail.com>
2024-10-24 12:44:11 +0300

    add a instructions

--- ChineseZodiac/solution.js
+++ ChineseZodiac/solution.js
@@ -1,4 +1,6 @@
 const animals = ["Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Snake", "Horse", "Goat", "Monkey", "Rooster", "Dog", "Pig"]
 const elements = ["Wood", "Fire", "Earth", "Metal", "Water"]
 
-console.log(animals[(1998 % 12) + 1])
+console.log(1998 - 1924)
+console.log((14 % 12) +1)
+console.log(14 % 5)
