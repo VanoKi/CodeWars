35c9ab8339283c7f8c4b9cace2d6709db4ed2f5e
VanoKi <kirsvano@gmail.com>
2024-10-24 10:52:15 +0300

    feat: check the conception

---
+++ ChineseZodiac/solution.js
@@ -0,0 +1,4 @@
+const animals = ["Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Snake", "Horse", "Goat", "Monkey", "Rooster", "Dog", "Pig"]
+const elements = ["Wood", "Fire", "Earth", "Metal", "Water"]
+
+console.log(animals[(1998 % 12) + 1])
