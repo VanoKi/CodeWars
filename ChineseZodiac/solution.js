c11998fa6642430d059249d6e1cb0c441b66bb70
VanoKi <kirsvano@gmail.com>
2024-10-24 13:09:16 +0300

    add a working solution

--- ChineseZodiac/solution.js
+++ ChineseZodiac/solution.js
@@ -1,6 +1,15 @@
 const animals = ["Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Snake", "Horse", "Goat", "Monkey", "Rooster", "Dog", "Pig"]
 const elements = ["Wood", "Fire", "Earth", "Metal", "Water"]
 
-console.log(1998 - 1924)
-console.log((14 % 12) +1)
-console.log(14 % 5)
+// console.log(1998 - 1924)
+// console.log((14 % 12) +1)
+// console.log(14 % 5)
+
+function chineseZodiac(year){
+    let cycle = year-1924
+    let animalIndex = (cycle%12)
+    let elementsIndex = Math.floor(cycle/2)%5
+    return `${elements[elementsIndex]} ${animals[animalIndex]}`
+}
+
+console.log(chineseZodiac(2016))
