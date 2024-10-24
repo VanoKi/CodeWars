92eaab293803e42ee14100f89f3b770d7ce4cc44
VanoKi <kirsvano@gmail.com>
2024-10-24 21:09:58 +0300

    feat: add a first variant solution

--- ConsonantValue/solution.js
+++ ConsonantValue/solution.js
@@ -1,8 +1,22 @@
 const ic = require('node-icecream')()
 
-const vovels = "aeiou"
+const vowels = "aeiou"
 const abc = []
 for (let i = 96; i < 123; i++) {
     abc.push(String.fromCharCode(i))
 }
-ic(abc)
+// ic(abc)
+let ans = []
+let seq = ''
+let word = 'zodiac'
+for (let i = 0; i < word.length; i++) {
+    if(!vowels.includes(word[i])){
+        seq += word[i]
+    }else{
+        ans.push(seq)
+        seq = ''
+    }
+}
+ans.push(seq)
+ic(seq)
+ic(ans)
