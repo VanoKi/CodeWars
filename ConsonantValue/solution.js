d0583e9f1379e088ada633c366be4c183c515129
VanoKi <kirsvano@gmail.com>
2024-10-24 21:37:06 +0300

    feat: add a new function sumIndexes

--- ConsonantValue/solution.js
+++ ConsonantValue/solution.js
@@ -1,7 +1,7 @@
 const ic = require('node-icecream')()
 
 const vowels = "aeiou"
-const abc = []
+const abc = 0
 for (let i = 96; i < 123; i++) {
     abc.push(String.fromCharCode(i))
 }
@@ -13,10 +13,20 @@ for (let i = 0; i < word.length; i++) {
     if(!vowels.includes(word[i])){
         seq += word[i]
     }else{
-        ans.push(seq)
+        let sumIndex = sumIndexes(seq)
+        if(sumIndex > ans){
+            ans = sumIndex
+        }
         seq = ''
     }
 }
 ans.push(seq)
 ic(seq)
 ic(ans)
+
+const sumIndexes = (seq) => {
+  return [...seq].map(item => abc.indexOf(item))
+      .reduce((sum, current) => sum + current, 0)
+}
+
+// ic(sumIndexes('str'))
