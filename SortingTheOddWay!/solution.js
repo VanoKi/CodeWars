53e522a48161574ad5ee875a86023e93b9093b48
VanoKi <kirsvano@gmail.com>
2024-10-26 19:52:06 +0300

    feat: add a solution to the sort the odd way

--- SortingTheOddWay!/solution.js
+++ SortingTheOddWay!/solution.js
@@ -1,1 +1,8 @@
 const ic = require('node-icecream')()
+
+let arr = [11,22,33,44,55,55,90.4,4,78]
+let odd = arr.filter( i => i % 2 !== 0)
+    .sort( (a, b) => a - b)
+let even = arr.filter(i => i % 2 == 0)
+    .sort( (a, b) => b - a)
+ic([...odd, ...even])
