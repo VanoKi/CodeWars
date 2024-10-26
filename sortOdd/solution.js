e63b2ae1fcd4eb5e44ff2829d9f827102a90373c
VanoKi <kirsvano@gmail.com>
2024-10-26 19:33:49 +0300

    feat: correct the solution of the sortOdd

--- sortOdd/solution.js
+++ sortOdd/solution.js
@@ -1,10 +1,12 @@
 const ic = require('node-icecream')()
 
-let arr = [5, 8, 6, 3, 4]
+let arr = [ 11, 1, 2, 8, 3, 4, 5 ]
 let odd = arr.filter(item => !(item % 2) == 0)
+    .sort( (a, b) => ( a - b))
+ic(odd)
 let ans = []
 for (let i = 0; i < arr.length; i++) {
-    ans.push(arr[i] % 2 == 0 ? arr[i] : odd.pop())
+    ans.push(arr[i] % 2 == 0 ? arr[i] : odd.shift())
 }
 
 ic(ans)
