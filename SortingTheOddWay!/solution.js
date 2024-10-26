ff32b771f2d6a5b0e1dd48fd27176fd064024072
VanoKi <kirsvano@gmail.com>
2024-10-26 20:00:20 +0300

    feat: correct the solution of the Sorting the Odd way

--- SortingTheOddWay!/solution.js
+++ SortingTheOddWay!/solution.js
@@ -1,8 +1,14 @@
 const ic = require('node-icecream')()
 
-let arr = [11,22,33,44,55,55,90.4,4,78]
-let odd = arr.filter( i => i % 2 !== 0)
+let arr = [
+    28.6, 98.2, 66.8,   32, 40.9,
+    11.2, 28.9, 16.5,  5.4,  3.9,
+    75.1, 37.1,   67, 20.5, 58.6,
+    6.4, 72.2, 75.9, 11.2, 30.6,
+    60.6, 34.3
+]
+let odd = arr.filter( i => Math.floor(i) % 2 !== 0)
     .sort( (a, b) => a - b)
-let even = arr.filter(i => i % 2 == 0)
+let even = arr.filter(i => Math.floor(i) % 2 == 0)
     .sort( (a, b) => b - a)
 ic([...odd, ...even])
