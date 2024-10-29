2e1232fd515f93385c3d459f50820fa9141c3cde
VanoKi <kirsvano@gmail.com>
2024-10-29 17:54:00 +0300

    add solution

--- SortWithAsortingArray/solution.js
+++ SortWithAsortingArray/solution.js
@@ -1,2 +1,9 @@
 const ic = require('node-icecream')()
 
+let a = ['z', 'x', 'y'] // ['z', 'y', 'x']
+let b = [0, 2, 1]
+let ans = []
+for (let i = 0; i < a.length; i++) {
+    ans[b[i]] = a[i]
+}
+ic(ans)
