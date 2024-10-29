c330dedaf50c1d5b964590b26775f514306e7bb5
VanoKi <kirsvano@gmail.com>
2024-10-29 15:58:42 +0300

    first loop

--- SortingArrays7kyu/solution.js
+++ SortingArrays7kyu/solution.js
@@ -1,5 +1,12 @@
-const ic = requires('node-icecream')()
+const ic = require('node-icecream')()
 
 const a1 = ['giraffe', 'orangutan', 'impala', 'elephant', 'rhino']
 const a2 = ['rattlesnake', 'eagle', 'geko', 'iguana', 'octopus']
 
+const ans = []
+const d = {}
+for (let i = 0; i < a1.length; i++) {
+    for (let j = 0; j < a2.length; j++) {
+        ic(a1[i], a2[j])
+    }
+}
