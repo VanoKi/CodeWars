f7df9d9ef2b55ec5a9abba5d7128baf78734887e
VanoKi <kirsvano@gmail.com>
2024-10-29 16:07:31 +0300

    feat: add work solition

--- SortingArrays7kyu/solution.js
+++ SortingArrays7kyu/solution.js
@@ -7,6 +7,10 @@ const ans = []
 const d = {}
 for (let i = 0; i < a1.length; i++) {
     for (let j = 0; j < a2.length; j++) {
-        ic(a1[i], a2[j])
+        if(a1[i][0] == a2[j][0]) {
+            ans[i] = a2[j]
+        }
     }
 }
+
+ic(ans)
