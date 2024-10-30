e6f503f61d094788df3460c604f30c065bbaa252
VanoKi <kirsvano@gmail.com>
2024-10-30 17:23:13 +0300

    feat: true  solution

--- CreditCardMask/solution.js
+++ CreditCardMask/solution.js
@@ -1,7 +1,7 @@
 const ic = require('node-icecream')()
 
 function maskify(cc) {
-    return cc.length > 4 ? cc.slice(-4) : cc
+    return cc.length > 4 ? '#'.repeat(cc.slice(0, -4).length) + cc.slice(-4) : cc
 }
 
-ic(maskify('616'))
+ic(maskify('4556364607935616'))
