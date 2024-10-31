df26ae61494c0aea111e667c522b552b39d5a090
VanoKi <kirsvano@gmail.com>
2024-10-31 15:08:34 +0300

    add a solution from another repo

--- CreditCardChecker/solution.js
+++ CreditCardChecker/solution.js
@@ -1,2 +1,20 @@
 const ic = require('node-icecream')()
 
+function validCard(card){
+    const arr = [...String(card)].map((item) => +item);
+    const ans = [];
+    for (let i = arr.length - 1; i >= 0; i -= 1) {
+        if ((arr.length - 1 - i) % 2 !== 0) {
+            if (arr[i] * 2 > 9) {
+                ans.push([...String(arr[i] * 2)].reduce((s, c) => s + +c, 0));
+            } else {
+                ans.push(arr[i] * 2);
+            }
+        } else {
+            ans.push(arr[i]);
+        }
+    }
+    return ans.reduce((s, c) => s + c, 0) % 10 === 0;
+}
+
+ic(validCard("1234 5678 9012 3452"))
