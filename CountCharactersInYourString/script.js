15c3066221fd33db988a450d170bcd199d8d5878
VanoKi <kirsvano@gmail.com>
2024-11-01 14:02:36 +0300

    feat: add solution

--- CountCharactersInYourString/script.js
+++ CountCharactersInYourString/script.js
@@ -1,8 +1,15 @@
 const ic = require('node-icecream')()
 
 function count(string) {
-    // TODO
-    return {};
+    let d = {}
+    for (let i = 0; i < string.length; i++) {
+        if(string[i] in d) {
+            d[string[i]] += 1
+        }else{
+            d[string[i]] = 1
+        }
+    }
+    return d;
 }
 
-ic(count())
+ic(count('aba'))
