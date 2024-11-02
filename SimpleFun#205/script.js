c0ae526f713d16fa8171715392ef0adcdc6497f9
VanoKi <kirsvano@gmail.com>
2024-11-02 15:19:18 +0300

    try new varion of solving

--- SimpleFun#205/script.js
+++ SimpleFun#205/script.js
@@ -1,11 +1,5 @@
 const ic = require('node-icecream')()
 
 let n = "7970521.5544"
-let number = +n
-// let len =
-// ic(+n)
-while (n > 0) {
-    ic(n % 10)
-    n = Math.floor(n/10)
-    ic(n)
-}
+let [leftPart, rigtPart] = n.split('.')
+ic(leftPart, rigtPart)
