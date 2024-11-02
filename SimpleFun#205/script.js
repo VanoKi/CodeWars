eb9e6ead09a097e626856752546c2347075723c4
VanoKi <kirsvano@gmail.com>
2024-11-02 15:33:28 +0300

    continue solving by second way

--- SimpleFun#205/script.js
+++ SimpleFun#205/script.js
@@ -3,3 +3,12 @@ const ic = require('node-icecream')()
 let n = "7970521.5544"
 let [leftPart, rigtPart] = n.split('.')
 ic(leftPart, rigtPart)
+let ans = []
+// for (let i = 0; i < leftPart.length; i++) {
+//     ans.push(leftPart[i] + '0'.repeat(leftPart.length - i - 1))
+// }
+// ic(ans)
+for (let i = 0; i < rigtPart.length; i++) {
+    ans.push('0'.repeat(rigtPart.length - i - 1) + rigtPart[i])
+}
+ic(ans)
