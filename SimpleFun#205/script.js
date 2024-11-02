1fe87499df9be4be7ffe5f1ede28371b37a47e89
VanoKi <kirsvano@gmail.com>
2024-11-02 15:03:11 +0300

    first variant of kata solving

--- SimpleFun#205/script.js
+++ SimpleFun#205/script.js
@@ -1,3 +1,11 @@
 const ic = require('node-icecream')()
 
-ic(1+1)
+let n = "7970521.5544"
+let number = +n
+// let len =
+// ic(+n)
+while (n > 0) {
+    ic(n % 10)
+    n = Math.floor(n/10)
+    ic(n)
+}
