55d8f53ad6cd5da389534fc3680543dd49a88c78
VanoKi <kirsvano@gmail.com>
2024-11-02 16:27:02 +0300

    try third way

--- SimpleFun#205/script.js
+++ SimpleFun#205/script.js
@@ -2,7 +2,7 @@ const ic = require('node-icecream')()
 
 let n = "7970521.5544"
 let [leftPart, rightPart] = n.split('.')
-ic(leftPart, rightPart)
+// ic(leftPart, rightPart)
 let ans = []
 for (let i = 0; i < leftPart.length; i++) {
     ans.push(leftPart[i] + '0'.repeat(leftPart.length - i - 1))
@@ -13,4 +13,12 @@ if(rightPart){
         ans.push('.' + '0'.repeat(i) + rightPart[i])
     }
 }
-ic(ans)
+// ic(ans.map(i => +i))
+let numb = +leftPart;
+let len = leftPart.length
+while (len > 0) {
+    ic(Math.floor(numb))
+    numb = numb / 10
+    // ic(numb % 10)
+    len--
+}
