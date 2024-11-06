439e67d76f97cb4bd28f0d247bbe1a84c33a5083
VanoKi <kirsvano@gmail.com>
2024-11-07 01:11:43 +0300

    fix: change second solution after consultation

--- OdderThanTheRest/solution.js
+++ OdderThanTheRest/solution.js
@@ -1,13 +1,13 @@
 const ic = require('node-icecream')()
 
 function oddOne(arr) {
-    let ans
     for (let i = 0; i < arr.length; i++) {
+        ic(i, arr[i] % 2)
         if (arr[i] % 2 !== 0){
-            ans = i
+            return i
         }
     }
-    return ans || -1
+    return -1
 }
 
-ic(oddOne([2,4,6,8]))
+ic(oddOne([-24,-68,30,-38,64,12,-50,-76,-32,86,17,36,-58,12,48,-30,86,2,-10,-76,78,46,-66,38,12,-38,18,48,-46,-60,-84,46,4,40,98,-88,-76,-18,-30]))
