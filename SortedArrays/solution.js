d4493dbfe5ded62f6c5e8c9db554a1f5c20cf3e5
VanoKi <kirsvano@gmail.com>
2024-11-07 00:30:23 +0300

    feat: add solution

--- SortedArrays/solution.js
+++ SortedArrays/solution.js
@@ -1,7 +1,10 @@
 const ic = require('node-icecream')()
 
-function nthSmallest(...arrays, n) {
-    return arrays
+function nthSmallest(...arrays) {
+    const n = arrays.pop()
+    const resArr = arrays.flat()
+        .sort((a, b) => a - b)
+    return resArr[n-1]
 }
 
-ic(nthSmallest([1,5], [2], [4,8,9]), 5)
+ic(nthSmallest([1,5], [2], [4,8,9], 4))
