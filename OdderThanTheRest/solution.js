90e6c3f48d99c45f3798bf8201ca4d95ff1a7adb
VanoKi <kirsvano@gmail.com>
2024-11-07 00:54:41 +0300

    feat: add second solutuion

--- OdderThanTheRest/solution.js
+++ OdderThanTheRest/solution.js
@@ -1,7 +1,13 @@
 const ic = require('node-icecream')()
 
 function oddOne(arr) {
-    return arr.filter(item => item % 2 !== 0)
+    let ans
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 !== 0){
+            ans = i
+        }
+    }
+    return ans || -1
 }
 
-ic(oddOne([2,4,6,7,10]))
+ic(oddOne([2,4,6,8]))
