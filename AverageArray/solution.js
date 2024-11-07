801001f0555cdfba8d9f30128d3c8230afc25243
VanoKi <kirsvano@gmail.com>
2024-11-07 11:22:03 +0300

    feat: try forEach

--- AverageArray/solution.js
+++ AverageArray/solution.js
@@ -4,10 +4,12 @@ function avgArray(arr) {
     let [a, b] = [...arr]
     let ans = []
     // return a.map((item) => (a[item] + b[item]))
-    for (let i = 0; i < a.length; i++) {
-        ic((a[i] + b[i]) / 2)
-    }
-
+    // for (let i = 0; i < a.length; i++) {
+    //     ic((a[i] + b[i]) / 2)
+    // }
+    a.forEach((val, i) => {
+        ic((val + b[i])/2)
+    })
 }
 
 ic(avgArray([[1, 2, 3, 4], [5, 6, 7, 8]]))
