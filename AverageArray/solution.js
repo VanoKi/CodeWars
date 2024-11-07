04a817c88357532effb6c7aa5ef7c97282fe58f2
VanoKi <kirsvano@gmail.com>
2024-11-07 12:08:09 +0300

    feat: try new approaches

--- AverageArray/solution.js
+++ AverageArray/solution.js
@@ -1,13 +1,14 @@
 const ic = require('node-icecream')()
 
 function avgArray(arr) {
-    let [a, b] = [...arr]
-    let ans = []
-    // a.forEach((val, i) => {
-    //     ans.push((val + b[i])/2)
-    // })
-    ans = a.map((val, i) => (a[i] + b[i]) / 2)
-    return ans
+    let ans = arr[0]
+    // ic(ans)
+    for (let i = 1; i < arr.length; i++) {
+        // ic(arr[i])
+        ans = ans.map((value, index) => value + arr[i][index]);
+        // ic(ans)
+    }
+    return ans.map(elem => elem / ans.length)
 }
 
-ic(avgArray([[1, 2, 3, 4], [5, 6, 7, 8]]))
+ic(avgArray([[2, 3, 9, 10, 7], [12, 6, 89, 45, 3], [9, 12, 56, 10, 34], [67, 23, 1, 88, 34]]))
