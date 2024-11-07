351be8561c2675b1b97225a7076a3e9123245166
VanoKi <kirsvano@gmail.com>
2024-11-07 11:31:44 +0300

    feat: try map

--- AverageArray/solution.js
+++ AverageArray/solution.js
@@ -3,13 +3,11 @@ const ic = require('node-icecream')()
 function avgArray(arr) {
     let [a, b] = [...arr]
     let ans = []
-    // return a.map((item) => (a[item] + b[item]))
-    // for (let i = 0; i < a.length; i++) {
-    //     ic((a[i] + b[i]) / 2)
-    // }
-    a.forEach((val, i) => {
-        ic((val + b[i])/2)
-    })
+    // a.forEach((val, i) => {
+    //     ans.push((val + b[i])/2)
+    // })
+    ans = a.map((val, i) => (a[i] + b[i]) / 2)
+    return ans
 }
 
 ic(avgArray([[1, 2, 3, 4], [5, 6, 7, 8]]))
