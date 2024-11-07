46aea8ccfd4d04faa885e56963a187b114bfa84f
VanoKi <kirsvano@gmail.com>
2024-11-07 12:16:07 +0300

    fшix arr.length

--- AverageArray/solution.js
+++ AverageArray/solution.js
@@ -2,13 +2,10 @@ const ic = require('node-icecream')()
 
 function avgArray(arr) {
     let ans = arr[0]
-    // ic(ans)
     for (let i = 1; i < arr.length; i++) {
-        // ic(arr[i])
         ans = ans.map((value, index) => value + arr[i][index]);
-        // ic(ans)
     }
-    return ans.map(elem => elem / ans.length)
+    return ans.map(elem => elem / arr.length)
 }
 
 ic(avgArray([[2, 3, 9, 10, 7], [12, 6, 89, 45, 3], [9, 12, 56, 10, 34], [67, 23, 1, 88, 34]]))
