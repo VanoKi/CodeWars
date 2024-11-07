74368e12eed144c6d7cc13b63043c1a2e389f6e8
VanoKi <kirsvano@gmail.com>
2024-11-07 13:03:13 +0300

    feat: add solution

--- SumOfArrayAverages/Solution.js
+++ SumOfArrayAverages/Solution.js
@@ -1,2 +1,9 @@
 const ic = require('node-icecream')()
 
+const sumAverage = (arr) => {
+    return Math.floor(arr
+        .map((val, i) => val.reduce((s, c) => s +c) / val.length)
+        .reduce((s,c) => s + c))
+}
+
+ic(sumAverage([[3, 4, 1, 3, 5, 1, 4], [21, 54, 33, 21, 77]]))
