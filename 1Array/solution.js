05b6f862e6aa75a4f0e6df431ec755cb62fd75c7
VanoKi <kirsvano@gmail.com>
2024-12-11 00:21:40 +0300

    feat: second solution +1array

--- 1Array/solution.js
+++ 1Array/solution.js
@@ -1,9 +1,15 @@
 const ic = require('node-icecream')()
 
-let arr = [0,7]
+let arr = [0, 7]
 
 function upArray(arr){
-    return (+arr.join('') + 1).toString().split('').map(item => +item)
+    for (let i = arr.length - 1; i >= 0; i--) {
+        arr[i] += 1
+        if (arr[i] < 10) break
+        arr[i] = 0
+    }
+    if (arr[0] === 0) arr.unshift(1)
+    return arr
 }
 
 ic(upArray(arr))
