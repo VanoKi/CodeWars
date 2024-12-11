c32273e5e4f200e37d39968cf99304e0c3a38700
VanoKi <kirsvano@gmail.com>
2024-12-11 15:29:43 +0300

    fix: check integer in +1array

--- 1Array/solution.js
+++ 1Array/solution.js
@@ -3,22 +3,24 @@ const ic = require('node-icecream')()
 let arr99 = [9, 9]
 let arr07 = [0, 7]
 let arr19 = [1, -9]
+let arrEmpty = []
 
 function upArray(arr){
-    for (let i = arr.length - 1; i >= 0; i--) {
-        if ( arr[i] < 10 && arr[i] >= 0) {
+    if (arr.length === 0) return null
+    if (!arr.every( item => item < 10 && item > -1)){
+        return null
+    } else{
+        for (let i = arr.length - 1; i >= 0; i--) {
             arr[i] += 1
             if (arr[i] < 10) {
                 return arr
             } else {
                 arr[i] = 0
             }
-        } else {
-            return null
         }
+        arr.unshift(1)
+        return arr
     }
-    arr.unshift(1)
-    return arr
 }
 
-ic(upArray(arr19))
+ic(upArray([]))
