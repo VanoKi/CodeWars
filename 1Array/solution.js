c6e91f5c97df530a548c87ce9562dce1da180962
VanoKi <kirsvano@gmail.com>
2024-12-11 14:30:42 +0300

    feat: first solution +1array

--- 1Array/solution.js
+++ 1Array/solution.js
@@ -1,15 +1,19 @@
 const ic = require('node-icecream')()
 
-let arr = [0, 7]
+let arr99 = [9, 9]
+let arr07 = [0, 7]
 
 function upArray(arr){
     for (let i = arr.length - 1; i >= 0; i--) {
         arr[i] += 1
-        if (arr[i] < 10) break
-        arr[i] = 0
+        if (arr[i] < 10) {
+            return arr
+        } else {
+            arr[i] = 0
+        }
     }
-    if (arr[0] === 0) arr.unshift(1)
+    arr.unshift(1)
     return arr
 }
 
-ic(upArray(arr))
+ic(upArray(arr99))
