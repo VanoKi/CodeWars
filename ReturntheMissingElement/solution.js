e32fe1a6878943149c4ee5fac87cba8b6b1dc3e5
VanoKi <kirsvano@gmail.com>
2024-12-13 23:35:12 +0300

    feat: kata solved

--- ReturntheMissingElement/solution.js
+++ ReturntheMissingElement/solution.js
@@ -1,2 +1,16 @@
 const ic = require('node-icecream')()
 
+function getMissingElement(superImportantArray){
+    // ic(superImportantArray.length)
+    for (let i = 0; i < superImportantArray.sort((a, b) => a - b).length +1; i++) {
+        if (i !== superImportantArray[i]) {
+            return i
+        }
+        // ic(i, superImportantArray[i])
+    }
+}
+
+ic(getMissingElement([
+    2, 5, 6, 9, 1,
+    3, 8, 4, 7
+]))
