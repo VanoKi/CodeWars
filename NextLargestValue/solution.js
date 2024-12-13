b461deda0ad4e873f35cf3510d09e49c9cd237c9
VanoKi <kirsvano@gmail.com>
2024-12-14 01:46:06 +0300

    feat: solve kata

--- NextLargestValue/solution.js
+++ NextLargestValue/solution.js
@@ -1,2 +1,9 @@
 const ic = require('node-icecream')()
 
+function largestValue(number){
+    const a = Array.from(number.toString()).reverse().join('');
+    const b = a.slice(0, a.length -2).concat(a[a.length -1]).concat(a[a.length - 2])
+    return [+a, +a < +b ? +b : +a]
+}
+
+ic(largestValue(95659512354))
