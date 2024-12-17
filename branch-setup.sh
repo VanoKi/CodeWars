6bfcc4e0865592a08d4fa7c234a40a507fe45c52
VanoKi <kirsvano@gmail.com>
2024-12-17 23:22:18 +0300

    feat: add a kata sequence generator

--- branch-setup.sh
+++ branch-setup.sh
@@ -1,6 +1,13 @@
+echo "Running branch-setup script..."
 BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
-mkdir -p "BRANCH_NAME"
-touch "$BRANCH_NAME/details.md"
-touch "$BRANCH_NAME/solution.js"
-echo "const ic = require('node-icecream')()" > "$BRANCH_NAME/solution.js"
+echo "Current branch: $BRANCH_NAME"
+if mkdir -p "$BRANCH_NAME"; then
+	echo 'Directory "$BRANCH_NAME" created successfully'
+else
+	echo 'Failed to create directory "$BRANCH_NAME".'
+fi
 
+mkdir -p "BRANCH_NAME"
+touch "$BRANCH_NAME/details.md" && echo "Created details.js"
+touch "$BRANCH_NAME/solution.js" && echo "Created solution.md"
+echo "const ic = require('node-icecream')()" > "$BRANCH_NAME/solution.js"
