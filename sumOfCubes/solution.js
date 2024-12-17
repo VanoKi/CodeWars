b31e4694d45601e6c8bbca51b632e0795f5f0fe3
VanoKi <kirsvano@gmail.com>
2024-12-17 21:44:12 +0300

    feat: add working solution

--- sumOfCubes/solution.js
+++ sumOfCubes/solution.js
@@ -1,2 +1,13 @@
 const ic = require('node-icecream')()
 
+function sumCubes(n){
+    let ans = 0
+    for (let i = 1; i < n + 1; ++i) {
+        ans += i ** 3
+    }
+    return ans
+}
+
+let n = 2
+
+ic(sumCubes(n))
