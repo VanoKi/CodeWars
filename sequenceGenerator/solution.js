66d13f8026817b36e4573ae2c46657089d9df1da
VanoKi <kirsvano@gmail.com>
2024-12-18 00:33:35 +0300

    feat: add solution

--- sequenceGenerator/solution.js
+++ sequenceGenerator/solution.js
@@ -1,2 +1,12 @@
 const ic = require('node-icecream')()
 
+function sequence(n, pattern) {
+    let arr = Array.from({length: n})
+    if (typeof pattern === 'function') {
+        return arr.map((i, idx) => pattern(i, idx))
+    } else {
+        return arr.fill(pattern)
+    }
+}
+
+ic(sequence(3, "s"))
