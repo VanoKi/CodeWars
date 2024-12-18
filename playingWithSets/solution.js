04fcf1ca975c3da28ef7196a3d721e179e316f06
VanoKi <kirsvano@gmail.com>
2024-12-18 16:09:14 +0300

    feat: add first solution

--- playingWithSets/solution.js
+++ playingWithSets/solution.js
@@ -1,2 +1,14 @@
 const ic = require('node-icecream')()
 
+function inter(A, B){
+    let result = []
+    for (const elem of A) {
+        if(B.has(elem)) result.push(elem)
+    }
+    return new Set(result)
+}
+
+let A =  new Set([1, 2])
+let B = new Set([2,3])
+
+ic(inter(A, B))
