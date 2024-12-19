d844adc9a41f24c0da5f2cc7155c976e4db4e182
VanoKi <kirsvano@gmail.com>
2024-12-20 01:10:20 +0300

    feat: add working solution

--- PalindromesWithASlightTwist/solution.js
+++ PalindromesWithASlightTwist/solution.js
@@ -11,20 +11,16 @@ function palindrome(str) {
         } else {
             if (strElement.codePointAt(0) > 64 && strElement.codePointAt(0) < 91) {
                 consonantInStr.push(strElement)
+                }
             }
         }
+    const isPalindrome = (arr) => {
+        return arr.toString() === arr.reverse().toString()
     }
-    const isPalindrome = (arr) => arr === arr.reverse()
-    if(isPalindrome(vowelsInStr) + isPalindrome(consonantInStr) == 2) {
-        return "both"
-    } else if(isPalindrome(vowelsInStr) + isPalindrome(consonantInStr) == 1) {
-            return isPalindrome(vowelsInStr) ? "vowel" : "consonant"
-        } else {
-        return "neither"
-        }
-    }
+    const answer = ['neither', [isPalindrome(vowelsInStr) ? 'vowels' : 'consonant'], 'both']
+    const result = answer[isPalindrome(vowelsInStr) + isPalindrome(consonantInStr)]
+    return Array.isArray(result) ? result[0] : result
+}
 
-
-
-let str = "pea13ce"
+let str = "wizard"
 ic(palindrome(str))
