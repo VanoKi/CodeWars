919f09ccf8eae18eca3e6485ead568c405773d43
VanoKi <kirsvano@gmail.com>
2024-12-20 18:45:44 +0300

    feat: add first solution

--- MatrixAddition/solution.js
+++ MatrixAddition/solution.js
@@ -1,1 +1,20 @@
 const ic = require('node-icecream')()
+
+function matrixAddition(a, b){
+    for (let i = 0; i < a.length; i++) {
+        for (let j = 0; j < a[i].length; j++) {
+            a[i][j] += b[i][j]
+        }
+    }
+    return a
+}
+
+let a = [ [1, 2, 3],
+    [3, 2, 1],
+    [1, 1, 1] ]
+
+let b = [ [2, 2, 1],
+    [3, 2, 3],
+    [1, 1, 3] ]
+
+ic(matrixAddition(a, b))
