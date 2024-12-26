d04166b9ca70415d6d6e00f4d6e1a5544c1763d0
VanoKi <kirsvano@gmail.com>
2024-12-26 16:13:59 +0300

    feat: add a previous solution

--- colorOfTheMoment/solution.js
+++ colorOfTheMoment/solution.js
@@ -1,2 +1,16 @@
 const ic = require('node-icecream')()
 
+function hexToTime(hexCode) {
+    let ans = []
+    for (let i = 1; i < hexCode.length; i += 2) {
+        let time = parseInt(hexCode.slice(i, i + 2), 16)
+        if (time > 59) {
+            throw new Error('Thats not a valid time!')
+        } else {
+            ans.push(time)
+        }
+    }
+    return ans[0] >= 24 ? 'Thats not a valid time!' : ans.join(':')
+}
+
+ic(hexToTime('#2c3721'))
