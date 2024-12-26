4b06e8c38ee81f5ab00e1f52da9e075ced08f689
VanoKi <kirsvano@gmail.com>
2024-12-26 16:27:03 +0300

    feat: add solution

--- colorOfTheMoment/solution.js
+++ colorOfTheMoment/solution.js
@@ -1,16 +1,21 @@
 const ic = require('node-icecream')()
 
 function hexToTime(hexCode) {
+    if (hexCode.length !== 7 || hexCode[0] !== '#') {
+        throw new Error(`Invalid hex code format! `)
+    }
     let ans = []
     for (let i = 1; i < hexCode.length; i += 2) {
         let time = parseInt(hexCode.slice(i, i + 2), 16)
-        if (time > 59) {
-            throw new Error('Thats not a valid time!')
-        } else {
-            ans.push(time)
-        }
+        ans.push(time)
     }
-    return ans[0] >= 24 ? 'Thats not a valid time!' : ans.join(':')
+
+    let [hours, minutes, seconds] = ans
+    if (hours >= 24 || minutes > 59 || seconds > 59) {
+        throw new Error('Thats not a valid time!')
+    }
+
+    return ans.map(num => num.toString()).join(':')
 }
 
-ic(hexToTime('#2c3721'))
+ic(hexToTime('#0d3737'))
