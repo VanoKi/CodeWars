f26321ffe519968f37e8debb38c1d31f6189478e
VanoKi <kirsvano@gmail.com>
2024-12-31 16:15:05 +0300

    fix: move to function, add check

--- simpleStringExpansion/solution.js
+++ simpleStringExpansion/solution.js
@@ -1,14 +1,20 @@
 const ic = require('node-icecream')()
 
-let string = ""
+// let string = "3D2a5d2f"
 // let string = "3abc"
-let ans = ''
-let arr = string.match(/\d[A-Za-z]*/ig)
-for (let i = 0; i < arr.length; i++) {
-    let symbols = arr[i].match(/[A-Za-z]*/g)[1]
-    let digit = arr[i].match(/\d/g)
-    for (let j = 0; j < symbols.length; j++) {
-        ans += symbols[j].repeat(+digit)
+
+function stringExpansion(s) {
+    let ans = ''
+    if(s.match(/\d*/g).join('') === '') return s
+    let arr = s.match(/\d[A-Za-z]*/ig)
+    for (let i = 0; i < arr.length; i++) {
+        let chanck = arr[i]
+        let symbols = chanck.slice(1)
+        let digit = chanck[0]
+        for (let j = 0; j < symbols.length; j++) {
+            ans += symbols[j].repeat(+digit)
+        }
     }
+    return ans
 }
-ic(ans)
+ic(stringExpansion("a2bcde"))
