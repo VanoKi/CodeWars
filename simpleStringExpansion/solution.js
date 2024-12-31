2e00db4f1ca8f78855d39f7a55a23c2e81040ebf
VanoKi <kirsvano@gmail.com>
2024-12-31 12:03:07 +0300

    fix: add check next cymbol in if block

--- simpleStringExpansion/solution.js
+++ simpleStringExpansion/solution.js
@@ -1,10 +1,12 @@
 const ic = require('node-icecream')()
 
-let string = "3D2a5d2f"
+let string = "3d332f2a"
 let ans = ''
 for (let i = 0; i < string.length - 1; i++) {
-    if(!isNaN(string[i])) {
+    if(!isNaN(string[i]) && isNaN(string[i + 1])) {
         ans += string[i+1].repeat(+string[i])
+    } else {
+        ans += string[i]
     }
 }
 
