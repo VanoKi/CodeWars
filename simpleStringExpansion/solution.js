3f1040307b82dba8fa302f0644d9dddf8020817d
VanoKi <kirsvano@gmail.com>
2024-12-31 15:59:16 +0300

    fix: add loops

--- simpleStringExpansion/solution.js
+++ simpleStringExpansion/solution.js
@@ -1,7 +1,14 @@
 const ic = require('node-icecream')()
 
-let string = "3D2a5d2f"
+let string = ""
 // let string = "3abc"
 let ans = ''
-
-ic(string.match(/\d[A-Za-z]*/ig))
+let arr = string.match(/\d[A-Za-z]*/ig)
+for (let i = 0; i < arr.length; i++) {
+    let symbols = arr[i].match(/[A-Za-z]*/g)[1]
+    let digit = arr[i].match(/\d/g)
+    for (let j = 0; j < symbols.length; j++) {
+        ans += symbols[j].repeat(+digit)
+    }
+}
+ic(ans)
