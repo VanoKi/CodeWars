528952f92a21cece10dda275e3a8d9539e1d1be2
VanoKi <kirsvano@gmail.com>
2024-12-31 15:45:29 +0300

    fix: use regex

--- simpleStringExpansion/solution.js
+++ simpleStringExpansion/solution.js
@@ -1,13 +1,7 @@
 const ic = require('node-icecream')()
 
-let string = "3abc"
+let string = "3D2a5d2f"
+// let string = "3abc"
 let ans = ''
-for (let i = 0; i < string.length - 1; i++) {
-    if(!isNaN(string[i]) && isNaN(string[i + 1])) {
-        ans += string[i+1].repeat(+string[i])
-    } else {
-        continue
-    }
-}
 
-ic(ans)
+ic(string.match(/\d[A-Za-z]*/ig))
