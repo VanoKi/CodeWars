3300f9ced30f4bad8cca122868bf08944a7d46d2
VanoKi <kirsvano@gmail.com>
2024-12-31 11:52:23 +0300

    feat: first solution

--- simpleStringExpansion/solution.js
+++ simpleStringExpansion/solution.js
@@ -1,1 +1,11 @@
 const ic = require('node-icecream')()
+
+let string = "3D2a5d2f"
+let ans = ''
+for (let i = 0; i < string.length - 1; i++) {
+    if(!isNaN(string[i])) {
+        ans += string[i+1].repeat(+string[i])
+    }
+}
+
+ic(ans)
