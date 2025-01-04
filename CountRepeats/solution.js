884dfa37336d6fcc1e096b1c7673eb8bb2f15006
VanoKi <kirsvano@gmail.com>
2025-01-04 21:40:09 +0300

    feat: solve the kata

--- CountRepeats/solution.js
+++ CountRepeats/solution.js
@@ -1,7 +1,13 @@
 const ic = require('node-icecream')()
 
 function countRepeats(str) {
-    // code solution
+    let count = 0
+    for (let i = 1; i < str.length; i++) {
+        if (str[i-1] === str[i]) {
+            count++
+        }
+    }
+    return count
 }
 
-ic(countRepeats())
+ic(countRepeats('ab cca'))
