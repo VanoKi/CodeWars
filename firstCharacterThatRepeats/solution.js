42a4e8361ba0ef4d14adacd9caf4108a08a3148f
VanoKi <kirsvano@gmail.com>
2025-01-04 22:09:12 +0300

    feat: move code to function

--- firstCharacterThatRepeats/solution.js
+++ firstCharacterThatRepeats/solution.js
@@ -1,20 +1,20 @@
 const ic = require('node-icecream')()
 
-let str = 'like'
-let dic = {}
-for (let i = 0; i < str.length; i++) {
-    if (str[i] in dic) {
-        dic[str[i]] += 1
-    } else {
-        dic[str[i]] = 1
+function firstDup (str) {
+    let dic = {}
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] in dic) {
+            dic[str[i]] += 1
+        } else {
+            dic[str[i]] = 1
+        }
+    }
+
+    for (let i = 0; i < str.length; i++) {
+        if (dic[str[i]] > 1){
+            return str[i]
+        }
     }
 }
 
-for (let i = 0; i < str.length; i++) {
-    if (dic[str[i]] > 1){
-        ic(str[i])
-        break
-    }
-}
-
-// ic(dic)
+ic(firstDup('bar'))
