dad99579b0e769843bab7f4a4de75f2aca84df35
VanoKi <kirsvano@gmail.com>
2025-01-04 22:01:13 +0300

    feat: second iteration of solving

--- firstCharacterThatRepeats/solution.js
+++ firstCharacterThatRepeats/solution.js
@@ -1,2 +1,13 @@
 const ic = require('node-icecream')()
 
+let str = 'tweet'
+let dic = {}
+for (let i = 0; i < str.length; i++) {
+    if (str[i] in dic) {
+        dic[str[i]] += 1
+    } else {
+        dic[str[i]] = 1
+    }
+}
+
+ic(dic)
