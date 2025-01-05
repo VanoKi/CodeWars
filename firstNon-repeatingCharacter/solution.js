0e33787ba1dd0b654cffa6f82e9c63a61c8cd69c
VanoKi <kirsvano@gmail.com>
2025-01-05 12:03:51 +0300

    feat: move code to function

--- firstNon-repeatingCharacter/solution.js
+++ firstNon-repeatingCharacter/solution.js
@@ -1,13 +1,16 @@
 const ic = require('node-icecream')()
 
-let str = 'sTreSS'
-let dic = {}
-let lowStr = str.toLowerCase()
-for (let i = 0; i < lowStr.length; i++) {
-    if (lowStr[i] in dic) dic[lowStr[i]] ++
-    else dic[lowStr[i]] = 1
+function firstNonRepeatingLetter(str) {
+    let dic = {}
+    let lowStr = str.toLowerCase()
+    for (let i = 0; i < lowStr.length; i++) {
+        if (lowStr[i] in dic) dic[lowStr[i]] ++
+        else dic[lowStr[i]] = 1
+    }
+    for (let i = 0; i < str.length; i++) {
+        if (dic[str[i].toLowerCase()] === 1) return str[i]
+    }
+    return ''
 }
-for (let i = 0; i < str.length; i++) {
-    if (dic[str[i].toLowerCase()] === 1) ic(str[i])
-}
-// ic(dic)
+
+ic(firstNonRepeatingLetter('moonmen'))
