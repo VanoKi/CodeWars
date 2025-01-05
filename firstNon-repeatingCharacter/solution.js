d1ca15386e400578dbb3242259787d294f4c6825
VanoKi <kirsvano@gmail.com>
2025-01-05 12:00:29 +0300

    feat: add first iteration of solution

--- firstNon-repeatingCharacter/solution.js
+++ firstNon-repeatingCharacter/solution.js
@@ -1,1 +1,13 @@
 const ic = require('node-icecream')()
+
+let str = 'sTreSS'
+let dic = {}
+let lowStr = str.toLowerCase()
+for (let i = 0; i < lowStr.length; i++) {
+    if (lowStr[i] in dic) dic[lowStr[i]] ++
+    else dic[lowStr[i]] = 1
+}
+for (let i = 0; i < str.length; i++) {
+    if (dic[str[i].toLowerCase()] === 1) ic(str[i])
+}
+// ic(dic)
