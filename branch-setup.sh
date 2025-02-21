0a22f6868b4208155940d148832348a4f51f96bb
VanoKi <kirsvano@gmail.com>
2025-02-21 20:26:42 +0300

    fix: branch-setup.sh

--- branch-setup.sh
+++ branch-setup.sh
@@ -2,12 +2,12 @@ echo "Running branch-setup script..."
 BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
 echo "Current branch: $BRANCH_NAME"
 if mkdir -p "$BRANCH_NAME"; then
-	echo 'Directory "$BRANCH_NAME" created successfully'
+	echo "Directory \"$BRANCH_NAME\" created successfully"
 else
-	echo 'Failed to create directory "$BRANCH_NAME".'
+	echo "Failed to create directory "$BRANCH_NAME"."
 fi
 
-mkdir -p "BRANCH_NAME"
-touch "$BRANCH_NAME/details.md" && echo "Created details.js"
-touch "$BRANCH_NAME/solution.js" && echo "Created solution.md"
+mkdir -p "$BRANCH_NAME"
+touch "$BRANCH_NAME/details.md" && echo "Created details.md"
+touch "$BRANCH_NAME/solution.js" && echo "Created solution.js"
 echo "const ic = require('node-icecream')()" > "$BRANCH_NAME/solution.js"
