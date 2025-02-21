fe6c8a306e0f79fbacfa473cd041a0fac714ae45
VanoKi <kirsvano@gmail.com>
2025-02-21 10:48:19 +0300

    feat: add function

--- MakeClass/solution.js
+++ MakeClass/solution.js
@@ -1,1 +1,22 @@
 const ic = require('node-icecream')()
+
+function makeClass(...properities) {
+    return properities
+}
+
+const list = ["name","species","age","health","weight","color"]
+
+// class Animal {
+//     constructor(name, species) {
+//         this.name = name;
+//         this.species = species;
+//     }
+//     say() {
+//         return `Hi! I'm ${this.name} and I'm a ${this.species}`
+//     }
+// }
+
+// const dog = new Animal('bat', 'dog')
+// ic(dog.say())
+
+ic(makeClass("name","species","age","health","weight","color"))
