0f56801d48bf367c47ef406fe289b0823f737ed6
VanoKi <kirsvano@gmail.com>
2025-02-21 11:53:51 +0300

    feat: final solution

--- MakeClass/solution.js
+++ MakeClass/solution.js
@@ -1,25 +1,24 @@
 const ic = require('node-icecream')()
 
 function makeClass(...properities) {
-    const list = properities
-    const that = {}
-    for (const element of list) {
-        that[element] = element
+    return function(...args) {
+        properities.forEach((prop, index) => {
+            this[prop] = args[index]
+        })
     }
-    return that
 }
 
 // ic(makeClass("name","species","age","health","weight","color"))
-ic(new makeClass('Bob','Dog','5','good','50lb','brown'))
+// ic(new makeClass('Bob','Dog','5','good','50lb','brown'))
 
-// function Animal(name,species,age,health,weight,color) {
-//     this.name = name;
-//     this.species = species;
-//     this.age = age;
-//     this.health = health;
-//     this.weight = weight;
-//     this.color = color;
-//   }
-//   const Animel = makeClass('name','species','age','health','weight','color')
+function Animal(name,species,age,health,weight,color) {
+    this.name = name;
+    this.species = species;
+    this.age = age;
+    this.health = health;
+    this.weight = weight;
+    this.color = color;
+  }
+  const Animel = makeClass('name','species','age','health','weight','color')
 
-// ic(new Animel('Bob','Dog','5','good','50lb','brown'))
+ic(new Animel('Bob','Dog','5','good','50lb','brown'))
