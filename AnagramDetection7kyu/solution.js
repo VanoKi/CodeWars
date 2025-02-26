6a9a464017e8be08c0db90192e55ace75762630b
VanoKi <kirsvano@gmail.com>
2025-02-26 11:15:59 +0300

    feat: first solution

--- AnagramDetection7kyu/solution.js
+++ AnagramDetection7kyu/solution.js
@@ -1,5 +1,8 @@
 const ic = require('node-icecream')()
 
 var isAnagram = function(test, original) {
-
+    var sorted = (s ) => [...s].sort().join('');
+    return sorted(test) === sorted(original)
 };
+
+ic(isAnagram('Buckethead', 'DeathCubeK'))
