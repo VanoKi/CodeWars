ae0127b641e05f02060e32f7c4251cc897eb6c32
VanoKi <kirsvano@gmail.com>
2025-03-07 11:36:56 +0300

    feat: add solution to isograms

--- Isograms_TS_7/solution.ts
+++ Isograms_TS_7/solution.ts
@@ -1,2 +1,13 @@
-const ic = require('node-icecream')()
+function isIsogram(str: string): boolean{
+    const word: string = str.toLocaleLowerCase()
+    const stack: string[] = []
+    for (let index = 0; index < word.length; index++) {
+        if (stack.includes(word[index])) {
+            return false;
+        }
+        stack.push(word[index])
+    }
+    return true
+}
 
+console.log(isIsogram("Dermatoglyphics"));
