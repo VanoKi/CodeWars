b496b9d5e44bd2385f0488022323ad812bfe242f
VanoKi <kirsvano@gmail.com>
2025-03-07 11:36:56 +0300

    feat: add solution to isograms

--- Isograms_TS_7/solution.js
+++ Isograms_TS_7/solution.js
@@ -1,1 +1,12 @@
-const ic = require('node-icecream')()
+function isIsogram(str) {
+    var word = str.toLocaleLowerCase();
+    var stack = [];
+    for (var index = 0; index < word.length; index++) {
+        if (stack.includes(word[index])) {
+            return false;
+        }
+        stack.push(word[index]);
+    }
+    return true;
+}
+console.log(isIsogram("moOse"));
