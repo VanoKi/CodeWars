15887a3bd515c54babf273a9c1c4a5b137eca223
VanoKi <kirsvano@gmail.com>
2025-03-10 18:04:57 +0300

    feat: use file expression

--- FunwithES6Classes#2-AnimalsandInheritance/solution.js
+++ FunwithES6Classes#2-AnimalsandInheritance/solution.js
@@ -1,16 +1,25 @@
 const ic = require('node-icecream')()
 
-class User {
-    constructor(name) {
-        this.name = name
-    }
-    SayHI() {
-        ic(this.name)
+// class User {
+//     constructor(name) {
+//         this.name = name
+//     }
+//     SayHI() {
+//         ic(this.name)
+//     }
+// }
+//
+// let user = new User('Ivan')
+// ic(user)
+// user.SayHI()
+// ic(User.prototype.SayHI)
+// ic(Object.getOwnPropertyNames(User.prototype))
+
+let User = class {
+    sayHi(){
+        ic('Hi')
     }
 }
 
-let user = new User('Ivan')
-ic(user)
-user.SayHI()
-ic(User.prototype.SayHI)
-ic(Object.getOwnPropertyNames(User.prototype))
+let user = new User('John')
+user.sayHi()
