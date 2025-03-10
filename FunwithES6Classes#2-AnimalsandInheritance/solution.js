9fae89a9c24c0a8c093117307f74be43cb82373e
VanoKi <kirsvano@gmail.com>
2025-03-10 22:51:25 +0300

    feat: add solution

--- FunwithES6Classes#2-AnimalsandInheritance/solution.js
+++ FunwithES6Classes#2-AnimalsandInheritance/solution.js
@@ -15,11 +15,36 @@ class Animal {
 
 class Shark extends Animal{
     constructor(name, age, status) {
-        super(name, age, status);
+        super(name, age, 0, 'shark', status);
         this.species = 'shark'
         this.legs = 0
     }
 }
 
-let shark = new Shark('name', 'age', 'status')
-ic(shark.legs)
+class Cat extends Animal{
+    constructor(name, age, status) {
+        super(name, age, 4, 'cat', status);
+    }
+    introduce() {
+        return `Hello my name is ${this.name} and I am ${this.age} years old.  Meow meow!`
+    }
+}
+
+class Dog extends Animal{
+    constructor(name, age, status, master) {
+        super(name, age, 4, 'dog', status);
+        this.master = master
+    }
+    greetMaster() {
+        return `Hello ${this.master}`
+    }
+}
+
+let dog = new Dog('Dough', 12, "Serving his master", "Eliza")
+ic(dog.legs)
+ic(dog.name)
+ic(dog.age)
+ic(dog.species)
+ic(dog.status)
+ic(dog.master)
+ic(dog.introduce())
