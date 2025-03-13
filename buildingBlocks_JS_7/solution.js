1f1f06675aa852fb6bd46bf851aa9619568e7558
VanoKi <kirsvano@gmail.com>
2025-03-13 19:46:55 +0300

    feat: write constructor

--- buildingBlocks_JS_7/solution.js
+++ buildingBlocks_JS_7/solution.js
@@ -1,1 +1,10 @@
 const ic = require('node-icecream')()
+
+class Block {
+    constructor(width, length, height) {
+        this.width = width
+        this.length = length
+        this.height  = height
+    }
+
+}
