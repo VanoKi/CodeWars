9fd7211b0a6076dfc43d6d83b188e55e0aed6e21
VanoKi <kirsvano@gmail.com>
2025-03-14 17:25:49 +0300

    feat: add loop

--- TransposingASong_js_7/solution.js
+++ TransposingASong_js_7/solution.js
@@ -2,6 +2,25 @@ const ic = require('node-icecream')()
 
 function transpose(song, interval){
     // ...
+    const sharp = 'A, A#, B, C, C#, D, D#, E, F, F#, G, G#.'
+        .replaceAll(',', '')
+        .replace('.', '')
+        .split(' ')
+    const flat = 'A, Bb, B, C, Db, D, Eb, E, F, Gb, G, Ab.'
+        .replace('.', '')
+        .replaceAll(',', '')
+        .split(' ')
+    // return sharp[flat.indexOf(song)]
+    const move = (item) => {
+        let index = sharp.indexOf(song)
+        if (song.includes('b')) {
+            index = flat.indexOf(song)
+            return sharp[(index + interval) % 12]
+        }
+        return sharp[(index + interval) % 12]
+    }
+    // return sharp[(sharp.indexOf(song) + interval) % 12]
+    return song.map(item => move(item))
 }
 
-ic(transpose())
+ic(transpose(['Ab', 'Gb'], 2))
