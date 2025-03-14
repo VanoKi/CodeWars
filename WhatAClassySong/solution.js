2881d9da1fc6a0d66d639e92d390047d575ae5b2
VanoKi <kirsvano@gmail.com>
2025-03-14 14:31:38 +0300

    feat: start kata

---
+++ WhatAClassySong/solution.js
@@ -0,0 +1,13 @@
+const ic = require('node-icecream')()
+
+class Song {
+    constructor(title, artist) {
+        this.title = title
+        this.artist = artist
+    }
+    howMany(array) {
+
+    }
+}
+
+const mountMoose = new Song('Mount Moose', 'The Snazzy Moose')
