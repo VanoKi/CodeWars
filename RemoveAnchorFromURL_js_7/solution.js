dc8ee068c03fa9a704357a4c9ea92dde44ae836e
VanoKi <kirsvano@gmail.com>
2025-03-27 11:39:29 +0300

    feat: first solution

--- RemoveAnchorFromURL_js_7/solution.js
+++ RemoveAnchorFromURL_js_7/solution.js
@@ -1,2 +1,7 @@
 const ic = require('node-icecream')()
 
+function removeUrlAnchor(url){
+    return url.slice(0, url.indexOf('#'))
+}
+const string = 'www.codewars.com/katas/'
+ic(removeUrlAnchor(string))
