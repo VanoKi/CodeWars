1562f7d2eb147684d8b425cf0464f17b7d447f89
VanoKi <kirsvano@gmail.com>
2025-03-27 10:57:30 +0300

    feat: first solution

--- doesArrayContain_js_6/solution.js
+++ doesArrayContain_js_6/solution.js
@@ -1,1 +1,15 @@
 const ic = require('node-icecream')()
+
+Object.defineProperty( Array.prototype, "containsAll", { value: function containsAll(a) {
+        // return true || false ;
+        b.forEach(item => {
+            if (!this.includes(item)) return false
+        })
+        return true
+    } } );
+
+const a = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+const b = [1,2,3,4,5]
+
+let result = a.containsAll(b)
+ic(result)
