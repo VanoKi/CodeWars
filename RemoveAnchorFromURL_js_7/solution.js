610ea5c8671421a260ae180bb20ab9326e313524
VanoKi <kirsvano@gmail.com>
2025-03-27 11:49:28 +0300

    feat: add if

--- RemoveAnchorFromURL_js_7/solution.js
+++ RemoveAnchorFromURL_js_7/solution.js
@@ -1,7 +1,7 @@
 const ic = require('node-icecream')()
 
 function removeUrlAnchor(url){
-    return url.slice(0, url.indexOf('#'))
+    return url.includes('#') ? url.slice(0, url.indexOf('#')) : url
 }
 const string = 'www.codewars.com/katas/'
 ic(removeUrlAnchor(string))
