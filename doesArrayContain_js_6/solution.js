b036d6596c8e3f6348ca9331a59704192a3a5631
VanoKi <kirsvano@gmail.com>
2025-03-27 11:06:26 +0300

    fix: change forEach to standrat fori

--- doesArrayContain_js_6/solution.js
+++ doesArrayContain_js_6/solution.js
@@ -1,15 +1,18 @@
 const ic = require('node-icecream')()
 
-Object.defineProperty( Array.prototype, "containsAll", { value: function containsAll(a) {
-        // return true || false ;
-        b.forEach(item => {
-            if (!this.includes(item)) return false
-        })
+Object.defineProperty( Array.prototype, "containsAll", { value: function containsAll(array) {
+        // a.forEach(item => {
+        //     if(!this.includes(item)) return false
+        // })
+        for (let i = 0; i < array.length; i++) {
+            if (!this.includes(array[i])) return false
+        }
         return true
     } } );
 
 const a = [1,2,3,4,5,6,7,8,9,10,11,12,13]
 const b = [1,2,3,4,5]
+const c = [12, 134]
 
-let result = a.containsAll(b)
+let result = a.containsAll(c)
 ic(result)
