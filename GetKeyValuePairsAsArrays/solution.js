64a3d736d033bf65790f819c1a97e70f0fc22b54
VanoKi <kirsvano@gmail.com>
2025-03-27 14:55:19 +0300

    feat: add loop

--- GetKeyValuePairsAsArrays/solution.js
+++ GetKeyValuePairsAsArrays/solution.js
@@ -1,1 +1,13 @@
 const ic = require('node-icecream')()
+
+function keysAndValues(dict) {
+    // return dict.valueOf()
+    const keys = []
+    const values = []
+    for (const dictKey in dict) {
+        keys.push(dictKey)
+        values.push(dict[dictKey])
+    }
+    return [keys, values]
+}
+ic(keysAndValues({a: 1, b: 2, c: 3}))
