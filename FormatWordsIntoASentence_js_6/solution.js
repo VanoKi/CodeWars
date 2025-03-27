dfd087798325903b6a9c10221c031956abee1dd2
VanoKi <kirsvano@gmail.com>
2025-03-27 12:08:12 +0300

    feat: first solution

--- FormatWordsIntoASentence_js_6/solution.js
+++ FormatWordsIntoASentence_js_6/solution.js
@@ -1,1 +1,8 @@
 const ic = require('node-icecream')()
+
+function formatWords(words){
+    return `${words.slice(0, -1).join(', ')} and ${words.at(-1)}`
+    // return words.slice(0, -1)
+}
+let string = ['one', 'two', '']
+ic(formatWords(string))
