7bdac143e97f54ce6a829d1122620c75c9851cc8
VanoKi <kirsvano@gmail.com>
2025-03-27 13:37:59 +0300

    feat: check length before default

--- FormatWordsIntoASentence_js_6/solution.js
+++ FormatWordsIntoASentence_js_6/solution.js
@@ -3,15 +3,17 @@ const ic = require('node-icecream')()
 function formatWords(words) {
     let commas = words
         .filter(item => item && item.trim() !== '')
+    ic(commas)
     let commasLenght = commas.length
+    ic(commasLenght)
     const cases = {
         0: '',
         1: commas[0],
         2: commas.join(' and '),
         default:
-            commas.slice(0, -1).join(', ') + ' and ' + commas[commas.length - 1]
+            commas.length > 1 ? commas.slice(0, -1).join(', ') + ' and ' + commas.at(-1) : ''
     }
     return cases[commasLenght] || cases.default
 }
-let string = ['one', 'two', 'three']
+let string = null
 ic(formatWords(string))
