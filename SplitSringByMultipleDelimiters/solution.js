c900a8964b6f3da30d5bda881ad9bc4a7b22d51f
VanoKi <kirsvano@gmail.com>
2025-03-28 09:30:48 +0300

    feat: prototype

--- SplitSringByMultipleDelimiters/solution.js
+++ SplitSringByMultipleDelimiters/solution.js
@@ -1,1 +1,11 @@
 const ic = require('node-icecream')()
+
+function multipleSplit(string, delimiters=[]){
+    let ans = []
+    let word = ''
+    for (let i = 0; i < string.length; i++) {
+        if (delimiters.includes(string[i])) ic(i)
+    }
+}
+const string = 'Hi, how are you?'
+ic(multipleSplit(string, [' ']))
