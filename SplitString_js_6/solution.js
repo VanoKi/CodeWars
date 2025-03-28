1a6da64e6a0732c2f2f5b28f0f2f56da56fe94fa
VanoKi <kirsvano@gmail.com>
2025-03-28 11:57:24 +0300

    feat: add replace

--- SplitString_js_6/solution.js
+++ SplitString_js_6/solution.js
@@ -3,8 +3,7 @@ const ic = require('node-icecream')()
 function solution(str){
     let ans = []
     for (let i = 0; i < str.length; i += 2) {
-        ic(str[i], str[i+1])
-        // ans.push(str[i], str[i+1])
+        ans.push((str[i] + str[i+1]).replace('undefined', '_'))
     }
     return ans
 }
