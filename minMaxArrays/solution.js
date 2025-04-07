1c854469c9d7d530259ae11c3044bda98d6b90cb
VanoKi <kirsvano@gmail.com>
2025-04-07 15:25:27 +0300

    feat: sort array

--- minMaxArrays/solution.js
+++ minMaxArrays/solution.js
@@ -1,1 +1,9 @@
 const ic = require('node-icecream')()
+
+function solve(arr){
+    const sortedArray = arr.sort((a, b) => b - a)
+    return sortedArray
+};
+
+const array = [15,11,10,7,12]
+ic(solve(array))
