200eea5dab6797395ecedd54939cb34e639dc46f
VanoKi <kirsvano@gmail.com>
2025-04-07 15:34:04 +0300

    feat: add second index

--- minMaxArrays/solution.js
+++ minMaxArrays/solution.js
@@ -2,7 +2,13 @@ const ic = require('node-icecream')()
 
 function solve(arr){
     const sortedArray = arr.sort((a, b) => b - a)
-    return sortedArray
+    let ans = []
+    ic(sortedArray)
+    for (let i = 0; i < arr.length / 2; i++) {
+        // ic(arr[i], arr[arr.length - i + 1])
+        // ic(i, arr.length - i - 1)
+        // ic(arr[arr.length - i - 1])
+    }
 };
 
 const array = [15,11,10,7,12]
