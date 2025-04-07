9dcb311278ecd1ae0066d0ae85fee6353ecef34a
VanoKi <kirsvano@gmail.com>
2025-04-07 15:56:52 +0300

    feat: create a dictinary

--- countingDuplicats_js_6/solution.js
+++ countingDuplicats_js_6/solution.js
@@ -1,7 +1,15 @@
 const ic = require('node-icecream')()
 function duplicateCount (text) {
-  //
+  let dict = {}
+  for (let i = 0; i < text.length; i++) {
+    let item = text[i].toLowerCase()
+    if (item in dict) {
+      dict[item] += 1
+    } else {
+      dict[item] = 1
+    }
   }
-  ic(duplicateCount())
+  return dict
+}
+ic(duplicateCount("aabbcde"))
 
-let text = "aabbcde"
