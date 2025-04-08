a5523203bc12c02d8ec5db355600fa8cca0b2ffe
VanoKi <kirsvano@gmail.com>
2025-04-08 15:25:04 +0300

    feat: add double loop

--- mapOverListOfList_js_7/solution.js
+++ mapOverListOfList_js_7/solution.js
@@ -1,6 +1,11 @@
 const ic = require('node-icecream')()
-function nameFunction (params) {
-
+function nameFunction (fn, xss) {
+    const ans = []
+    for (let i = 0; i < xss.length; i++) {
+        for (let j = 0; j < xss[i].length; j++) {
+            ic(fn(xss[i][j]))
+        }
+    }
 }
-ic(nameFunction())
+ic(nameFunction(x => x.toUpperCase(),[['h','E','l','l','O'],['w','O','r','L','d']]))
 
