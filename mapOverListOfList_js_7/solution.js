d3034ca95ee2585bf9af9e927a4a274bfe1c2fa3
VanoKi <kirsvano@gmail.com>
2025-04-08 15:32:08 +0300

    feat: move innicialization of inner array into the outher array

--- mapOverListOfList_js_7/solution.js
+++ mapOverListOfList_js_7/solution.js
@@ -2,10 +2,13 @@ const ic = require('node-icecream')()
 function nameFunction (fn, xss) {
     const ans = []
     for (let i = 0; i < xss.length; i++) {
+        const subArray = []
         for (let j = 0; j < xss[i].length; j++) {
-            ic(fn(xss[i][j]))
+            subArray.push(fn(xss[i][j]))
         }
+        ans.push(subArray)
     }
+    return ans
 }
 ic(nameFunction(x => x.toUpperCase(),[['h','E','l','l','O'],['w','O','r','L','d']]))
 
