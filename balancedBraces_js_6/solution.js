050edfc5207db742b3acade37d8cbb0de8363ad7
VanoKi <kirsvano@gmail.com>
2025-04-15 14:45:54 +0300

    feat: add loop

--- balancedBraces_js_6/solution.js
+++ balancedBraces_js_6/solution.js
@@ -1,6 +1,21 @@
 const ic = require('node-icecream')()
-function isBalanced (params) {
-  //
+
+function isBalanced(str) {
+    const left = '({['
+    const right = ')}]'
+    let stack = []
+    let count = 0
+    for (let i = 0; i < str.length; i++) {
+        if (left.includes(str[i])) stack.push(str[i])
+        if (right.includes(str[i])) {
+            if(right.indexOf(str[i]) === left.indexOf(stack.at(-1))) {
+                count += 1
+                stack.pop()
+            }
+        }
+    }
+    return [stack, count]
 }
+
 ic(isBalanced("(string[5])"))
 
