4dc7ef5f788839546071039a1a3fdc2078414115
VanoKi <kirsvano@gmail.com>
2025-04-26 19:14:53 +0300

    feat: first solution

--- extractLastNamesOfPeopleNamedMichael/solution.js
+++ extractLastNamesOfPeopleNamedMichael/solution.js
@@ -1,6 +1,17 @@
 const ic = require('node-icecream')()
-function nameFunction (params) {
-  //
+
+function nameFunction(params) {
+    let array = str.split(' ')
+    const ans = []
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === 'Michael') {
+            let lastName = array[i + 1]
+            ans.push(lastName.replace(/[.,!?;:]+$/, ''))
+        }
+    }
+    return ans
 }
-ic(nameFunction())
+
+const str = "Michael, how are you? - Cool, how is John Williamns and Michael Jordan? I don't know but Michael Johnson is fine. Michael do you still score points with LeBron James, Michael Green AKA Star and Michael Wood?";
+ic(nameFunction(str))
 
