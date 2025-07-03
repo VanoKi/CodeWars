cbe14600cb49465d264263318cceb593cfc4d56b
VanoKi <kirsvano@gmail.com>
2025-07-03 13:24:15 +0300

    feat: flat array

--- DontDrinkTheWater_JS_5/solution.js
+++ DontDrinkTheWater_JS_5/solution.js
@@ -1,2 +1,10 @@
 const ic = require('node-icecream')()
 
+const glass = [['H', 'H', 'W', 'O'],['W','W','O','W'],['H','H','O','O']]
+
+function separateLiquids(glass) {
+
+    return glass.flat();
+}
+
+ic(separateLiquids(glass))
