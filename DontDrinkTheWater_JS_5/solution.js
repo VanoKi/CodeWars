b60b1beb030fc10c9be0cf54e0b5d0cf079a42c5
VanoKi <kirsvano@gmail.com>
2025-07-03 13:28:47 +0300

    feat: add sort

--- DontDrinkTheWater_JS_5/solution.js
+++ DontDrinkTheWater_JS_5/solution.js
@@ -1,10 +1,17 @@
 const ic = require('node-icecream')()
 
-const glass = [['H', 'H', 'W', 'O'],['W','W','O','W'],['H','H','O','O']]
+// const glass = [['H', 'H', 'W', 'O'],['W','W','O','W'],['H','H','O','O']]
+
+const density = {
+    'H': 1.36,
+    'W': 1.00,
+    'A': 0.87,
+    'O': 0.80
+}
 
 function separateLiquids(glass) {
 
-    return glass.flat();
+    return glass.flat().sort((a, b) => a.localeCompare(b));
 }
 
 ic(separateLiquids(glass))
