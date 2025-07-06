85686902286efff658b4513f65559da3035e6f2a
VanoKi <kirsvano@gmail.com>
2025-07-06 16:21:40 +0300

    fix: dictinaries

--- TheRoadKillDetective_js_5/solution.py
+++ TheRoadKillDetective_js_5/solution.py
@@ -1,15 +1,15 @@
 # from preloaded import ANIMALS
 from icecream import ic
-# ANIMALS = ['aardvark', 'alligator', 'armadillo', 'antelope', 'baboon', 'bear', 'bobcat', 'butterfly', 'cat', 'camel',
-#            'cow', 'chameleon', 'dog', 'dolphin', 'duck', 'dragonfly', 'eagle', 'elephant', 'emu', 'echidna', 'fish',
-#            'frog', 'flamingo', 'fox', 'goat', 'giraffe', 'gibbon', 'gecko', 'hyena', 'hippopotamus', 'horse', 'hamster',
-#            'insect', 'impala', 'iguana', 'ibis', 'jackal', 'jaguar', 'jellyfish', 'kangaroo', 'kiwi', 'koala',
-#            'killerwhale', 'lemur', 'leopard', 'llama', 'lion', 'monkey', 'mouse', 'moose', 'meercat', 'numbat', 'newt',
-#            'ostrich', 'otter', 'octopus', 'orangutan', 'penguin', 'panther', 'parrot', 'pig', 'quail', 'quokka',
-#            'quoll', 'rat', 'rhinoceros', 'racoon', 'reindeer', 'rabbit', 'snake', 'squirrel', 'sheep', 'seal', 'turtle',
-#            'tiger', 'turkey', 'tapir', 'unicorn', 'vampirebat', 'vulture', 'wombat', 'walrus', 'wildebeast', 'wallaby',
-#            'yak', 'zebra']
-ANIMALS = ['bear']
+ANIMALS = ['aardvark', 'alligator', 'armadillo', 'antelope', 'baboon', 'bear', 'bobcat', 'butterfly', 'cat', 'camel',
+           'cow', 'chameleon', 'dog', 'dolphin', 'duck', 'dragonfly', 'eagle', 'elephant', 'emu', 'echidna', 'fish',
+           'frog', 'flamingo', 'fox', 'goat', 'giraffe', 'gibbon', 'gecko', 'hyena', 'hippopotamus', 'horse', 'hamster',
+           'insect', 'impala', 'iguana', 'ibis', 'jackal', 'jaguar', 'jellyfish', 'kangaroo', 'kiwi', 'koala',
+           'killerwhale', 'lemur', 'leopard', 'llama', 'lion', 'monkey', 'mouse', 'moose', 'meercat', 'numbat', 'newt',
+           'ostrich', 'otter', 'octopus', 'orangutan', 'penguin', 'panther', 'parrot', 'pig', 'quail', 'quokka',
+           'quoll', 'rat', 'rhinoceros', 'racoon', 'reindeer', 'rabbit', 'snake', 'squirrel', 'sheep', 'seal', 'turtle',
+           'tiger', 'turkey', 'tapir', 'unicorn', 'vampirebat', 'vulture', 'wombat', 'walrus', 'wildebeast', 'wallaby',
+           'yak', 'zebra']
+# ANIMALS = ['bear']
 
 
 def road_kill(photo):
@@ -29,22 +29,22 @@ def road_kill(photo):
             return result, zipped
         result = []
         for i in ANIMALS:
-            animal = create_dict(i)[0]
-            victim = create_dict(photo)[0]
-            ic(animal, victim)
-            for j in animal:
-                if victim[j] - animal[j] == -1:
+            animal = create_dict(i)
+            victim = create_dict(photo)
+            ic(animal[0], victim[0],animal[1], victim[1])
+            for j in animal[0]:
+                if victim[0][j] - animal[0][j] == -1:
                     break
                 else:
+                    if victim[1] == animal[1] or victim[1][::-1] == animal[1]:
+                        return i
 
-
-
-    return create_dict(photo)
-
+    return '??'
+# change check !!!!!!!
 # ic(road_kill("=====k====r=a=vvvv==d=d=d=d=r==a=a======="))
 # ic(road_kill("===       ===snake========="))
-# ic(road_kill("==a======a=a=a=lig===a=t====o=r=r=r=r=="))
-ic(road_kill("=====r=rrr=rra=====eee======bb====b======="))
+ic(road_kill("==a======a=a=a=lig===a=t====o=r=r=r=r=="))
+# ic(road_kill("=====r=rrr=rra=====eee======bb====b======="))
 
 # def is_subsequence(word, sequence):
 #     it = iter(sequence)
