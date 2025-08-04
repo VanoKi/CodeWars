98bec3ebb10aa5e9137e5ab5eb557be57c8d929b
VanoKi <kirsvano@gmail.com>
2025-08-04 17:18:41 +0300

    feat: new approach

--- repeatAdjacent/solution.js
+++ repeatAdjacent/solution.js
@@ -1,33 +1,8 @@
 const ic = require('node-icecream')()
 function repeatAdjacent (s) {
-    s = s + ' '
-    let answe = 0
-    let result = []
-    let word = ''
-    for (const param of s) {
-        if (word) {
-            if (word.at(-1) === param) {
-                word += param
-            } else {
-                if (word.length >= 2) {
-                    result.push(word)
-                } else {
-                    if (result.length >= 2) {
-                        answe += 1
-                        result = []
-                    }else {
-                        result = []
-                    }
-                }
-                word = param
-            }
-        }
-        else {
-            word = param
-        }
+    for (let i = 0; i < s.length; i++) {
+        ic(s.slice(i, i + 3))
     }
-    if (word) result.push(word)
-    return answe
 }
-ic(repeatAdjacent("soooooldieeeeeer"))
+ic(repeatAdjacent("wwwwaaaarrioooorrrrr"))
 
