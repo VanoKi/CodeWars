52fd9fd3f9a82ee07ce49c3c274955f468ee155f
VanoKi <kirsvano@gmail.com>
2025-08-04 18:41:31 +0300

    feat: write test

--- repeatAdjacent/solution.js
+++ repeatAdjacent/solution.js
@@ -1,8 +1,18 @@
 const ic = require('node-icecream')()
 function repeatAdjacent (s) {
+    const ans = []
     for (let i = 0; i < s.length; i++) {
-        ic(s.slice(i, i + 3))
+        if(s[i] !== s[i+1] && s[i+1] !== s[i+2]) {
+            ic(s[i], s[i+1], s[i +2])
+            ans.push(s.slice(0, i + 2))
+            ic(ans)
+            s = s.slice(i + 2)
+            ic(s)
+        }
     }
+    ic(ans)
+    ans.push(s)
+    return ans
 }
 ic(repeatAdjacent("wwwwaaaarrioooorrrrr"))
 
---
+++ repeatAdjacent/test-solution.js
@@ -0,0 +1,24 @@
+import {test, expect} from 'bun:test'
+import {repeatAdjacent} from './solution'
+import {describe} from "test";
+
+function doTest(s, expected) {
+    const actual = repeatAdjacent(s)
+    expect(actual).toBe(expected)
+}
+
+describe("repeatAdjacent tests", () => {
+    test("sample tests", () => {
+        doTest("ccccoodeffffiiighhhhhhhhhhttttttts", 3);
+        doTest("soooooldieeeeeer", 0);
+        doTest("", 0);
+        doTest("aaaa", 0);
+        doTest("ccccoooooooooooooooooooooooddee", 1);
+        doTest("chaaallengee", 1);
+        doTest("wwwwaaaarrioooorrrrr", 2);
+        doTest(
+            "gztxxxxxggggggggggggsssssssbbbbbeeeeeeehhhmmmmmmmitttttttlllllhkppppp",
+            2
+        );
+    });
+});
