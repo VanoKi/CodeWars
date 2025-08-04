6a8890c9a7876e3a68a96430eaf4c1b38081cb9d
VanoKi <kirsvano@gmail.com>
2025-08-04 18:47:59 +0300

    fix: export to solution

--- repeatAdjacent/solution.js
+++ repeatAdjacent/solution.js
@@ -1,18 +1,18 @@
-const ic = require('node-icecream')()
-function repeatAdjacent (s) {
+// const ic = require('node-icecream')()
+export function repeatAdjacent (s) {
     const ans = []
     for (let i = 0; i < s.length; i++) {
         if(s[i] !== s[i+1] && s[i+1] !== s[i+2]) {
-            ic(s[i], s[i+1], s[i +2])
+            // ic(s[i], s[i+1], s[i +2])
             ans.push(s.slice(0, i + 2))
-            ic(ans)
+            // ic(ans)
             s = s.slice(i + 2)
-            ic(s)
+            // ic(s)
         }
     }
-    ic(ans)
+    // ic(ans)
     ans.push(s)
     return ans
 }
-ic(repeatAdjacent("wwwwaaaarrioooorrrrr"))
+// ic(repeatAdjacent("wwwwaaaarrioooorrrrr"))
 
