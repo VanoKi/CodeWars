aed5c9bc3362b6e95235144f33811fc19ab25de7
VanoKi <kirsvano@gmail.com>
2025-08-05 20:30:22 +0300

    feat: add shifting cycle

--- vovelShifting/solution.js
+++ vovelShifting/solution.js
@@ -1,16 +1,20 @@
 
 function nameFunction (s, num) {
   const vowels = "aeiouAEIOU"
-  let vowelsInString = []
-  let vowelsIndex = []
-  for (let i = 0; i < s.length; i++) {
-    if (vowels.includes(s[i])) {
-      vowelsInString.push(s[i])
-      vowelsIndex.push(i)
-    }
-  }
+  // let vowelsInString = []
+  // let vowelsIndex = []
+  // for (let i = 0; i < s.length; i++) {
+  //   if (vowels.includes(s[i])) {
+  //     vowelsInString.push(s[i])
+  //     vowelsIndex.push(i)
+  //   }
+  const vowelsInString = [1, 2, 3, 4]
+  // }
   const n = num % vowelsInString.length
-  return [vowelsInString, vowelsIndex, n, ]
+  for (let i = n; i < vowelsInString.length; i++) {
+    console.log(vowelsInString.slice(i), vowelsInString.slice(0, i))
+  }
+  // return [vowelsInString, vowelsIndex, n, ]
 }
 
 console.log(nameFunction("This is a string", 1))
