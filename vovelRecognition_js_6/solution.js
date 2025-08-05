bde7093133a535b01ae636f0ff7c06315f57b552
VanoKi <kirsvano@gmail.com>
2025-08-05 14:18:35 +0300

    feat: first loop

--- vovelRecognition_js_6/solution.js
+++ vovelRecognition_js_6/solution.js
@@ -1,4 +1,7 @@
 function nameFunction (params) {
+  for (let i = 0; i < params.length; i++) {
+    console.log(params[i], params.slice(0, i))
+  }
   return params
 }
 console.log(nameFunction("baceb"))
