727999324610ea762f2dddd0394667f0ddfe1c3b
VanoKi <kirsvano@gmail.com>
2025-08-05 14:37:44 +0300

    feat: add vovels

--- vovelRecognition_js_6/solution.js
+++ vovelRecognition_js_6/solution.js
@@ -1,8 +1,11 @@
+const vovels = 'aeiouAEIOU'
+ceAll(',', '')
 function nameFunction (params) {
   for (let i = 0; i < params.length; i++) {
     console.log(params[i], params.slice(0, i))
   }
   return params
 }
-console.log(nameFunction("baceb"))
+// console.log(nameFunction("baceb"))
+console.log(vovels)
 
