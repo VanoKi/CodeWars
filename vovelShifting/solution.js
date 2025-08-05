c9859bd2dc222e426349e4a3c9090dd15b9837dc
VanoKi <kirsvano@gmail.com>
2025-08-05 16:39:31 +0300

    feat: add shift

--- vovelShifting/solution.js
+++ vovelShifting/solution.js
@@ -1,17 +1,18 @@
 
-function nameFunction (s) {
+function nameFunction (s, n) {
   const vowels = "aeiouAEIOU"
   let vowelsInString = []
   let vowelsIndex = []
   for (let i = 0; i < s.length; i++) {
     if (vowels.includes(s[i])) {
       vowelsInString.push(s[i])
-      vowelsIndex.push(s[i])
+      vowelsIndex.push(i)
     }
   }
-
-  return [vowelsInString, vowelsIndex]
+  const num = vowelsInString.length % n
+  const shifted = vowelsInString.slice(-num).concat(vowelsInString.slice(0, -num))
+  return [vowelsInString, vowelsIndex, n, shifted]
 }
 
-console.log(nameFunction("This is a string"))
+console.log(nameFunction("This is a string", 1))
 
