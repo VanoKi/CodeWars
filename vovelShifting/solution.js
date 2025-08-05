9ef4bf658e43fbb71b126df478258cdca871d528
VanoKi <kirsvano@gmail.com>
2025-08-05 21:14:58 +0300

    feat: add condition

--- vovelShifting/solution.js
+++ vovelShifting/solution.js
@@ -11,8 +11,16 @@ function nameFunction (s, num) {
   }
   console.log(vowelsInString)
   console.log(vowelsIndex)
-  const n = num % vowelsInString.length
-  const shiftedVowels = vowelsInString.slice(n).concat(vowelsInString.slice(0, n))
+  let n
+  let shiftedVowels
+  if (num > 0) {
+    n = -(num % vowelsInString.length)
+    shiftedVowels = vowelsInString.slice(n).concat(vowelsInString.slice(0, n))
+  } else {
+    n = num % vowelsInString.length
+    shiftedVowels = vowelsInString.slice(n).concat(vowelsInString.slice(0, n))
+  }
+  console.log(shiftedVowels)
   let answer = Array.from(s)
   for (let i = 0; i < vowelsIndex.length; i++) {
     answer.splice(vowelsIndex[i], 1, shiftedVowels[i])
@@ -20,5 +28,6 @@ function nameFunction (s, num) {
   return answer.join('')
 }
 
+console.log("This is a test!")
 console.log(nameFunction("This is a test!", 1))
 
