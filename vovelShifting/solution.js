c613b411b4be07fe3628d03747b49b728be6f617
VanoKi <kirsvano@gmail.com>
2025-08-05 16:31:28 +0300

    feat: first loop

--- vovelShifting/solution.js
+++ vovelShifting/solution.js
@@ -1,6 +1,17 @@
 
-function nameFunction (params) {
-  return params
+function nameFunction (s) {
+  const vovels = "aeiouAEIOU"
+  let vovelsInString = []
+  let vovelsIndex = []
+  for (let i = 0; i < s.length; i++) {
+    if (vovels.includes(s[i])) {
+      vovelsInString.push(s[i])
+      vovelsIndex.push(s[i])
+    }
+  }
+
+  return [vovelsInString, vovelsIndex]
 }
-console.log(nameFunction())
+
+console.log(nameFunction("This is a string"))
 
