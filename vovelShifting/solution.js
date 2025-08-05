f25cb23602779160f0dad271a36722e284c1d45c
VanoKi <kirsvano@gmail.com>
2025-08-05 20:34:59 +0300

    fix: shiftedVowels

--- vovelShifting/solution.js
+++ vovelShifting/solution.js
@@ -1,20 +1,17 @@
 
 function nameFunction (s, num) {
   const vowels = "aeiouAEIOU"
-  // let vowelsInString = []
-  // let vowelsIndex = []
-  // for (let i = 0; i < s.length; i++) {
-  //   if (vowels.includes(s[i])) {
-  //     vowelsInString.push(s[i])
-  //     vowelsIndex.push(i)
-  //   }
-  const vowelsInString = [1, 2, 3, 4]
-  // }
-  const n = num % vowelsInString.length
-  for (let i = n; i < vowelsInString.length; i++) {
-    console.log(vowelsInString.slice(i), vowelsInString.slice(0, i))
+  let vowelsInString = []
+  let vowelsIndex = []
+  for (let i = 0; i < s.length; i++) {
+    if (vowels.includes(s[i])) {
+      vowelsInString.push(s[i])
+      vowelsIndex.push(i)
+    }
   }
-  // return [vowelsInString, vowelsIndex, n, ]
+  const n = num % vowelsInString.length
+  const shiftedVowels = vowelsInString.slice(n).concat(vowelsInString.slice(0, n))
+  return [vowelsInString, vowelsIndex, n, shiftedVowels]
 }
 
 console.log(nameFunction("This is a string", 1))
