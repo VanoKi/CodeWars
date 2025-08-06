cff0a814e4ade46d1b63aea079d0724e900e5bf1
VanoKi <kirsvano@gmail.com>
2025-08-06 16:27:35 +0300

    fix: del conditioin

--- vovelShifting/solution.js
+++ vovelShifting/solution.js
@@ -13,15 +13,8 @@ function vowelShift (s, num) {
   }
   log(vowelsInString)
   // log(vowelsIndex)
-  let n
-  let shiftedVowels
-  if (num > 0) {
-    n = -(num % vowelsInString.length)
-    shiftedVowels = vowelsInString.slice(n).concat(vowelsInString.slice(0, n))
-  } else {
-    n = num % vowelsInString.length
-    shiftedVowels = vowelsInString.slice(n).concat(vowelsInString.slice(0, n))
-  }
+  let n = -(num % vowelsInString.length)
+  let shiftedVowels = vowelsInString.slice(n).concat(vowelsInString.slice(0, n))
   log(shiftedVowels)
   let answer = Array.from(s)
   for (let i = 0; i < vowelsIndex.length; i++) {
@@ -31,27 +24,29 @@ function vowelShift (s, num) {
 }
 
 // log("This as e tist!")
-// log(vowelShift("This is a test!", 1))
+log(vowelShift("This is a test!", 1))
 // log(vowelShift("This is a test!", 3));  // "This as e tist!"
 // log(vowelShift("This is a test!", 0));  // "This as e tist!"
 // log(vowelShift(" ", 0));  // "This as e tist!"
 
 // log(nameFunction("This is a test!", 1));  // "Thes is i tast!"
-// log(nameFunction("This is a test!", -1)); // "Thas os e tist!"
+log(vowelShift("This is a test!", -1)); // "Thas os e tist!"
 // log(nameFunction("aeiou", 2));            // "ouaei"
 // log(nameFunction("aeiou", -2));           // "iouae"
 function shift(num) {
   const arr = [1, 2, 3, 4]
-  let n
-  if (num > 0) {
-    n = -(num % arr.length)
-    console.log(n)
-    return arr.slice(n).concat(arr.slice(0 ,n))
-  } else {
-    n = num % arr.length
-    console.log(n)
-    log(arr.slice(-1))
-    return arr.slice(n).concat(arr.slice(0, n))
-  }
+  let n = -(num % arr.length)
+  return arr.slice(n).concat(arr.slice(0, n))
+  // if (num > 0) {
+  //   // n = -(num % arr.length)
+  //   console.log(n)
+  //   return arr.slice(n).concat(arr.slice(0 ,n))
+  // } else {
+  //   // n = num % arr.length
+  //   console.log(n)
+  //   // log(arr.slice(-1))
+  //   return arr.slice(n).concat(arr.slice(0, n))
+  // }
 }
-log(shift(1))
+// log(shift(1))
+// log(shift(-1))
