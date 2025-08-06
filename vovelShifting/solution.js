25ed5c201e75e16859cb254d25b772acf19986b9
VanoKi <kirsvano@gmail.com>
2025-08-06 14:17:17 +0300

    feat: log use bind...

--- vovelShifting/solution.js
+++ vovelShifting/solution.js
@@ -1,3 +1,4 @@
+const log = console.log.bind(console)
 
 function nameFunction (s, num) {
   const vowels = "aeiouAEIOU"
@@ -9,8 +10,8 @@ function nameFunction (s, num) {
       vowelsIndex.push(i)
     }
   }
-  console.log(vowelsInString)
-  console.log(vowelsIndex)
+  // console.log(vowelsInString)
+  // console.log(vowelsIndex)
   let n
   let shiftedVowels
   if (num > 0) {
@@ -20,7 +21,7 @@ function nameFunction (s, num) {
     n = num % vowelsInString.length
     shiftedVowels = vowelsInString.slice(n).concat(vowelsInString.slice(0, n))
   }
-  console.log(shiftedVowels)
+  log(shiftedVowels)
   let answer = Array.from(s)
   for (let i = 0; i < vowelsIndex.length; i++) {
     answer.splice(vowelsIndex[i], 1, shiftedVowels[i])
@@ -28,6 +29,11 @@ function nameFunction (s, num) {
   return answer.join('')
 }
 
-console.log("This is a test!")
-console.log(nameFunction("This is a test!", 1))
+log("This is a test!")
+log(nameFunction("This is a test!", 3))
+log(nameFunction("This is a test!", 3));  // "This as e tist!"
 
+log(nameFunction("This is a test!", 1));  // "Thes is i tast!"
+log(nameFunction("This is a test!", -1)); // "Thas os e tist!"
+log(nameFunction("aeiou", 2));            // "ouaei"
+log(nameFunction("aeiou", -2));           // "iouae"
