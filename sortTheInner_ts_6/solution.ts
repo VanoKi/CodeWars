6bdcc78faf13a826d3c151323e48d038c75fd526
VanoKi <kirsvano@gmail.com>
2025-08-21 19:17:43 +0300

    fix: add condition

--- sortTheInner_ts_6/solution.ts
+++ sortTheInner_ts_6/solution.ts
@@ -2,10 +2,17 @@ function sortTheInnerContent(words:string):string
 {
   let ans = ''
   for (const word of words.split(' ')) {
-    let core = Array.from(word.slice(1, -1)).sort((a, b) => b.localeCompare(a)).join('')
-    ans += (word.slice(0, 1) +  core + word.slice(word.length - 1) + ' ')
+    if (word.length > 3 ) {
+      let core = Array.from(word.slice(1, -1)).sort((a, b) => b.localeCompare(a)).join('')
+      // console.log(`word: ${word}`)
+      // console.log(`core: ${core}`)
+      ans += (word.slice(0, 1) +  core + word.slice(word.length - 1) + ' ')
+    } else {
+      ans += (word + ' ')
+    }
   }
   return ans.trim()
 }
 
 console.log(sortTheInnerContent("sort the inner content in descending order"))
+console.log('bt zyxunkgccp r obl psrqffdai')
