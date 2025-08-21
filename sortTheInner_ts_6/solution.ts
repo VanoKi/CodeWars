961ee20614c827d18a43d2f18ff40005568a89f9
VanoKi <kirsvano@gmail.com>
2025-08-21 19:01:21 +0300

    feat: trim

--- sortTheInner_ts_6/solution.ts
+++ sortTheInner_ts_6/solution.ts
@@ -1,16 +1,11 @@
 function sortTheInnerContent(words:string):string
 {
-  // return words;
   let ans = ''
   for (const word of words.split(' ')) {
-    // console.log(word)
-    let core = Array.from(word.slice(1, -1)).sort((a, b) => b - a).join('')
-    console.log(core)
-    // ans += (word.slice(0, 1) +  core + word.slice(word.length - 1) + ' ')
+    let core = Array.from(word.slice(1, -1)).sort((a, b) => b.localeCompare(a)).join('')
+    ans += (word.slice(0, 1) +  core + word.slice(word.length - 1) + ' ')
   }
-  // words.split(' ').forEach(word => console.log(word))
-  return ans
+  return ans.trim()
 }
 
-// console.log(sortTheInnerContent("sort the inner content in descending order"))
-console.log(Array.from('abcd').sort((a, b) => b.localeCompare(a)))
+console.log(sortTheInnerContent("sort the inner content in descending order"))
