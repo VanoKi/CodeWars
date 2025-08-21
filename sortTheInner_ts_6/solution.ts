e9d94f1b94bc74ed442cb63e5ef65be702b00e1f
VanoKi <kirsvano@gmail.com>
2025-08-21 18:02:49 +0300

    feat: first loop

---
+++ sortTheInner_ts_6/solution.ts
@@ -0,0 +1,9 @@
+function sortTheInnerContent(words:string):string
+{
+  // return words;
+  // for (const word of words.split(' ')) {
+  //   console.log(word)
+  // }
+}
+
+console.log(sortTheInnerContent("sort the inner content in descending order"))
