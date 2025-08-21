1d865d39d3fa47ed2f710d1383a104f28649902c
VanoKi <kirsvano@gmail.com>
2025-08-21 18:08:07 +0300

    feat: reverse

--- sortTheInner_ts_6/solution.ts
+++ sortTheInner_ts_6/solution.ts
@@ -1,10 +1,17 @@
 function sortTheInnerContent(words:string):string
 {
   // return words;
-  // for (const word of words.split(' ')) {
-  //   console.log(word)
-  // }
-  words.split(' ').forEach(word => console.log(word))
+  let ans = ''
+  for (const word of words.split(' ')) {
+    // console.log(word)
+    if (word.length <= 3) {
+      ans += (word + ' ')
+    } else {
+      ans += (Array.from(word).reverse().join('') + ' ')
+    }
+  }
+  // words.split(' ').forEach(word => console.log(word))
+  return ans
 }
 
 console.log(sortTheInnerContent("sort the inner content in descending order"))
