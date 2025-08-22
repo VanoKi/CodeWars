18fcf9b0492b8328895567e97acdb172c8601e70
VanoKi <kirsvano@gmail.com>
2025-08-22 12:49:06 +0300

    feat: create Array fill

--- xMarksTheSpot_ts_6/solution.ts
+++ xMarksTheSpot_ts_6/solution.ts
@@ -1,5 +1,10 @@
 
-function nameFunction (5) {
-
+function nameFunction (n:number) {
+  // return n
+  const arr = []
+  // for (let i = 0; i <= n; i++) {
+  //   arr.push(Array.length(n))
+  // }
+  return Array(n).fill(0)
 }
-console.log(nameFunction())
+console.log(nameFunction(5))
