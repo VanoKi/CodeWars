ba2aeda06f8a9a42051cb33f7160ab525c332b91
VanoKi <kirsvano@gmail.com>
2025-08-22 12:59:35 +0300

    fix: classic for

--- xMarksTheSpot_ts_6/solution.ts
+++ xMarksTheSpot_ts_6/solution.ts
@@ -1,10 +1,20 @@
 
 function nameFunction (n:number) {
   // return n
-  const arr = Array(n).fill(Array(n).fill(0))
-  // for (let i = 0; i <= n; i++) {
-  //   arr.push(Array.length(n))
+  // const arr = Array(n).fill(Array(n).fill(0))
+  // for (let i = 0; i < arr.length; i++) {
+  //   console.log(arr[i])
   // }
+  // return arr
+  let arr = []
+  for (let i = 0; i < n; i++) {
+    // console.log()
+    let temp = []
+    for (let j = 0; j < n; j++) {
+      temp.push(0)
+    }
+    arr.push(temp)
+  }
   return arr
 }
 console.log(nameFunction(5))
