35152996c9f03d10efdb3c1618b19a3397f27ae8
VanoKi <kirsvano@gmail.com>
2025-08-22 12:52:04 +0300

    feat: create inner array

--- xMarksTheSpot_ts_6/solution.ts
+++ xMarksTheSpot_ts_6/solution.ts
@@ -1,10 +1,10 @@
 
 function nameFunction (n:number) {
   // return n
-  const arr = []
+  const arr = Array(n).fill(Array(n).fill(0))
   // for (let i = 0; i <= n; i++) {
   //   arr.push(Array.length(n))
   // }
-  return Array(n).fill(0)
+  return arr
 }
 console.log(nameFunction(5))
