e880d94a2e8fd3c7838d6f0c71824466a0ec9fd9
VanoKi <kirsvano@gmail.com>
2025-08-22 13:12:30 +0300

    feat: realise

--- xMarksTheSpot_ts_6/solution.ts
+++ xMarksTheSpot_ts_6/solution.ts
@@ -10,11 +10,20 @@ function nameFunction (n:number) {
   for (let i = 0; i < n; i++) {
     // console.log()
     let temp = []
-    for (let j = 0; j < n; j++) {
-      temp.push(0)
+    for (let j = 1; j <= n; j++) {
+      // console.log(i)
+      if (n - i == j || i + 1 == j) {
+        temp.push(1)
+      }else {
+        temp.push(0)
+      }
     }
     arr.push(temp)
   }
   return arr
 }
-console.log(nameFunction(5))
+// console.log(nameFunction(5))
+let answer = nameFunction(4)
+for (let i = 0; i < answer.length; i++) {
+  console.log(answer[i])
+}
