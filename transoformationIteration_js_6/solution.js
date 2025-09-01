48baa14ff11b05da101e321ba795ce34fc71482e
VanoKi <kirsvano@gmail.com>
2025-09-01 14:55:34 +0300

    feat: add join

--- transoformationIteration_js_6/solution.js
+++ transoformationIteration_js_6/solution.js
@@ -1,13 +1,17 @@
 
 function nameFunction (source, target) {
-  const ans = []
+  const ans = [source]
   const word = [...source]
   for (let i = 0; i < source.length; i++) {
-    console.log(ans.splice(i, 1, target[i]))
+    // console.log(ans.splice(i, 1, target[i]))
+    console.log(source[i], target[i])
+    word.splice(i, 1, target[i])
+    console.log(word)
+    ans.push(word.join(''))
   }
   return ans
 }
-// console.log(nameFunction('car','see'))
-let word = [...'see']
-let newWord = word.splice(1, 1, 'a')
-console.log(word, newWord)
+console.log(nameFunction('car','see'))
+// let word = [...'see']
+// let newWord = word.splice(1, 1, 'a')
+// console.log(word, newWord)
