493ffaf205e0f1feae452ed580599b463fd9de63
VanoKi <kirsvano@gmail.com>
2025-09-01 14:59:34 +0300

    fix: name function

--- transoformationIteration_js_6/solution.js
+++ transoformationIteration_js_6/solution.js
@@ -1,5 +1,5 @@
 
-function nameFunction (source, target) {
+function transform (source, target) {
   const ans = [source]
   const word = [...source]
   for (let i = 0; i < source.length; i++) {
@@ -13,7 +13,7 @@ function nameFunction (source, target) {
   }
   return ans
 }
-console.log(nameFunction('kata','math'))
+console.log(transform('kata','math'))
 // let word = [...'see']
 // let newWord = word.splice(1, 1, 'a')
 // console.log(word, newWord)
