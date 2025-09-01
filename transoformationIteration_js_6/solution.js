3e9982455a35125f65eaec94df98fe310b60b63e
VanoKi <kirsvano@gmail.com>
2025-09-01 14:02:00 +0300

    feat: first solution

--- transoformationIteration_js_6/solution.js
+++ transoformationIteration_js_6/solution.js
@@ -1,6 +1,10 @@
 
-function nameFunction (params) {
-  return params
+function nameFunction (source, target) {
+  const ans = []
+  for (let i = 0; i < source.length; i++) {
+    console.log([...source].splice(i, 1, target[i]))
+  }
+  return ans
 }
-console.log(nameFunction())
+console.log(nameFunction('car','see'))
 
