28671db3d3aa9db3d5ae043b81437da82fee0246
VanoKi <kirsvano@gmail.com>
2025-09-01 14:41:21 +0300

    feat: move one word to console

--- transoformationIteration_js_6/solution.js
+++ transoformationIteration_js_6/solution.js
@@ -1,10 +1,13 @@
 
 function nameFunction (source, target) {
-  const ans = [...source]
+  const ans = []
+  const word = [...source]
   for (let i = 0; i < source.length; i++) {
     console.log(ans.splice(i, 1, target[i]))
   }
   return ans
 }
-console.log(nameFunction('car','see'))
-
+// console.log(nameFunction('car','see'))
+let word = [...'see']
+let newWord = word.splice(1, 1, 'a')
+console.log(word, newWord)
