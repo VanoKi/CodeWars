c4acf35b5e0fe7397bfe81372bef2c0d96fd2584
VanoKi <kirsvano@gmail.com>
2025-10-17 20:59:39 +0300

    feat: add shift (2025-10-17 20:59:39)

--- Vowels-Back-js-6/solution.js
+++ Vowels-Back-js-6/solution.js
@@ -1,6 +1,11 @@
 
-function nameFunction (params) {
-  return params
+function nameFunction (params, shift) {
+  const abs = 'abcdefghijklmnopqrstuvwxyz'
+  let result = ''
+  for (let i = 0; i < params.length; i++) {
+    let index = abs.indexOf(params[i])
+    result += abs[((index + shift) % 26 + 26) % 26]
+  }
+  return result
 }
-console.log(nameFunction())
-
+console.log(nameFunction('codewars', 1))
