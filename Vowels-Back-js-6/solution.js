15c589b73f7b8d24d586305a58aa06a53fa4fcfe
VanoKi <kirsvano@gmail.com>
2025-10-17 21:12:23 +0300

    fix: shift (2025-10-17 21:12:23)

--- Vowels-Back-js-6/solution.js
+++ Vowels-Back-js-6/solution.js
@@ -1,11 +1,11 @@
 
-function nameFunction (params, shift) {
+function shift (params, shift) {
   const abs = 'abcdefghijklmnopqrstuvwxyz'
   let result = ''
   for (let i = 0; i < params.length; i++) {
     let index = abs.indexOf(params[i])
-    result += abs[((index + shift) % 26 + 26) % 26]
+    result += abs[Math.abs((index + shift) % 26)]
   }
   return result
 }
-console.log(nameFunction('codewars', 1))
+console.log(shift('codewars', -1))
