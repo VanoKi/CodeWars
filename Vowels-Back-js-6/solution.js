acc2536fa1b8c15134e2e6175fd0b8977c497d9b
VanoKi <kirsvano@gmail.com>
2025-10-18 13:08:29 +0300

    feat: add new function (2025-10-18 13:08:28)

--- Vowels-Back-js-6/solution.js
+++ Vowels-Back-js-6/solution.js
@@ -1,11 +1,31 @@
-
-function shift (params, shift) {
-  const abs = 'abcdefghijklmnopqrstuvwxyz'
-  let result = ''
-  for (let i = 0; i < params.length; i++) {
-    let index = abs.indexOf(params[i])
-    result += abs[Math.abs((index + shift) % 26)]
+const vowelsBack = (str) => {
+  const detectChar = (char) => {
+    if ('aeiu'.includes(char)) {
+      return -5
+    } else if ('co'.includes(char)) {
+      return -1
+    } else if ('d'.includes(char)) {
+      return -3
+    } else if ('e'.includes(char)) {
+      return -4
+    } else {
+      return 9
+    }
   }
-  return result
+
+  function shift (params, shift) {
+    const abs = 'abcdefghijklmnopqrstuvwxyz'
+    let result = ''
+    for (let i = 0; i < params.length; i++) {
+      let index = abs.indexOf(params[i])
+      result += abs[Math.abs((index + shift) % 26)]
+    }
+    return result
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    console.log(shift(str[i], detectChar(str[i])))
+  }
 }
-console.log(shift('codewars', -1))
+
+vowelsBack('codewars')
