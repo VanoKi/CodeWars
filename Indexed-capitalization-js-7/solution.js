74ed872e172c344c8730d3e560f3948159195782
VanoKi <kirsvano@gmail.com>
2025-10-18 21:01:19 +0300

    feat: first solution (2025-10-18 21:01:19)

--- Indexed-capitalization-js-7/solution.js
+++ Indexed-capitalization-js-7/solution.js
@@ -1,6 +1,14 @@
 
-function nameFunction (params) {
-  return params
+function nameFunction (str, arr) {
+  let result = ''
+  for (let i = 0; i < str.length; i++) {
+    if (arr.includes(i)) {
+      result += str[i].toUpperCase()
+    } else {
+      result += str[i]
+    }
+  }
+  return result
 }
-console.log(nameFunction())
+console.log(nameFunction("abcdef", [1,2,5] ))
 
