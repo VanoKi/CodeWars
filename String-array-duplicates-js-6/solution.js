78ce99c099eb89b987f7dda7cfdb8da03e841c7e
VanoKi <kirsvano@gmail.com>
2025-10-18 22:37:34 +0300

    feat: first solution (2025-10-18 22:37:34)

--- String-array-duplicates-js-6/solution.js
+++ String-array-duplicates-js-6/solution.js
@@ -1,6 +1,15 @@
 
-function nameFunction (params) {
-  return params
+function dup(array) {
+  function reduceChars (params) {
+    let ans = ''
+    for (let i = 0; i < params.length; i++) {
+      if (ans.at(-1) === params[i]) continue
+      else ans += params[i]
+    }
+    return ans
+  }
+
+  return array.map(element => reduceChars(element))
 }
-console.log(nameFunction())
+console.log(duplicates(["kelless","keenness"]))
 
