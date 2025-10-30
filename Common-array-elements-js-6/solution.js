be88fec515ffbfca984c74d396a2c35c3f94775f
VanoKi <kirsvano@gmail.com>
2025-10-30 17:52:18 +0300

    feat: add second loop (2025-10-30 17:52:18)

--- Common-array-elements-js-6/solution.js
+++ Common-array-elements-js-6/solution.js
@@ -1,5 +1,5 @@
 function common(a, b, c) {
-  console.log([a, b, c])
+  // console.log([a, b, c])
   const dictForElem = (elem) => {
     const dict = {}
     for (const elemElement of elem) {
@@ -9,7 +9,17 @@ function common(a, b, c) {
     return dict
   }
 
-  console.log(Array.from(arguments).map(elem => dictForElem(elem)))
+  const dictionaries = Array.from(arguments).map(elem => dictForElem(elem))
+  // console.log(dictionaries)
+  const firstDict = dictionaries[0]
+  const commonElem = {}
+  for (let i = 1; i < dictionaries.length; i++) {
+    // console.log(dictionaries[i])
+    for (const dictKey in dictionaries[i]) {
+      if(dictKey in firstDict) commonElem[dictKey] = firstDict[dictKey]
+    }
+  }
+  return commonElem
 }
 
-console.log(common( [1, 2,2,3], [5, 2,3,3], [7, 2,2,3]))
+console.log(common( [1,2,2,3],[5,3,2,2],[7,3,2,2]))
