2eaf5d8d42b3e1ef5a009ec3853596e4875eb67d
VanoKi <kirsvano@gmail.com>
2025-10-30 16:42:54 +0300

    feat: create dictionary (2025-10-30 16:42:53)

--- Common-array-elements-js-6/solution.js
+++ Common-array-elements-js-6/solution.js
@@ -1,24 +1,15 @@
 function common(a, b, c) {
   console.log([a, b, c])
-  let ans = []
-  for (const argument of arguments) {
-    let dict = {}
-    for (const argumentElement of argument) {
-      if (dict[argumentElement]) dict[argumentElement] += 1
-      else {
-        dict[argumentElement] = 1
-      }
+  const dictForElem = (elem) => {
+    const dict = {}
+    for (const elemElement of elem) {
+      if (dict[elemElement]) dict[elemElement] += 1
+      else dict[elemElement] = 1
     }
-    ans.push(dict)
+    return dict
   }
-  let count = {}
-  for (const dict of ans) {
-    for (const dictKey in dict) {
-      if (dictKey in count) count[dictKey] += 1
-      else count[dictKey] = 1
-    }
-  }
-  return Object.entries(count).filter(item => item[1] == ans.length).map(item => ans[0][item[0]] * +item[0]).reduce((sum, item) => sum + item)
+
+  console.log(Array.from(arguments).map(elem => dictForElem(elem)))
 }
 
-console.log(common([1,2,2,3],[5,3,2,2],[7,3,2,2]))
+console.log(common( [1, 2,2,3], [5, 2,3,3], [7, 2,2,3]))
