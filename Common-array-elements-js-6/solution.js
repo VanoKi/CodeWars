488272ac26ed5044d1a50d481dded4d37f2cab98
VanoKi <kirsvano@gmail.com>
2025-10-31 17:18:41 +0300

    feat: third loop (2025-10-31 17:18:41)

--- Common-array-elements-js-6/solution.js
+++ Common-array-elements-js-6/solution.js
@@ -15,15 +15,20 @@ function common(a, b, c) {
   for (const dictionary of dictionaries) {
     // console.log(dictionary)
     for (const key in dictionary) {
-      console.log(key)
+      // console.log(key)
       // console.log(dictionary[dictionaryKey])
       if (bigDic[key]) bigDic[key] += 1
       else bigDic[key] = 1
     }
   }
 
-  console.log(Object.entries(bigDic).filter(e => e[1] >= dictionaries.length))
-
+  const commonKeys = Object.entries(bigDic).filter(e => e[1] >= dictionaries.length).map(e => e[0])
+  for (const commonKey of commonKeys) {
+    console.log(dictionaries[0][commonKey], commonKey)
+  }
+  return commonKeys.reduce( (sum, elem) => {
+    return sum + (dictionaries[0][elem] * Number(elem))
+  }, 0)
 }
 
 console.log(common( [1,2,2,3],[5,3,2,2],[7,3,2,2]))
