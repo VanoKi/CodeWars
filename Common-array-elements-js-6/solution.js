0a83ca8b3672953687abdff699d0621008d76dc8
VanoKi <kirsvano@gmail.com>
2025-10-31 16:32:06 +0300

    feat: create a bigdic (2025-10-31 16:32:06)

--- Common-array-elements-js-6/solution.js
+++ Common-array-elements-js-6/solution.js
@@ -5,11 +5,21 @@ function common(a, b, c) {
       if (dict[elemElement]) dict[elemElement] += 1
       else dict[elemElement] = 1
     }
-    return Object.entries(dict)
+    // return Object.entries(dict)
+    return dict
   }
 
   const dictionaries = Array.from(arguments).map(elem => dictForElem(elem))
-  return dictionaries
+  console.log(dictionaries)
+  const bigDic = {}
+  for (const dictionary of dictionaries) {
+    // console.log(dictionary)
+    for (const dictionaryKey in dictionary) {
+      // console.log(dictionaryKey)
+      // console.log(dictionary[dictionaryKey])
+    }
+  }
+
 }
 
 console.log(common( [1,2,2,3],[5,3,2,2],[7,3,2,2]))
