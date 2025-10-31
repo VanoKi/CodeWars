06ca7e4cca4ea0bd769026b0ecce136a6489a28c
VanoKi <kirsvano@gmail.com>
2025-10-31 12:47:26 +0300

    fix: change dict (2025-10-31 12:47:26)

--- Common-array-elements-js-6/solution.js
+++ Common-array-elements-js-6/solution.js
@@ -1,27 +1,15 @@
 function common(a, b, c) {
-  // console.log([a, b, c])
   const dictForElem = (elem) => {
     const dict = {}
     for (const elemElement of elem) {
       if (dict[elemElement]) dict[elemElement] += 1
       else dict[elemElement] = 1
     }
-    return dict
+    return Object.entries(dict)
   }
 
   const dictionaries = Array.from(arguments).map(elem => dictForElem(elem))
-  // console.log(dictionaries)
-  const firstDict = dictionaries[0]
-  const commonElem = {}
-  for (let i = 1; i < dictionaries.length; i++) {
-    // console.log(dictionaries[i])
-    for (const dictKey in dictionaries[i]) {
-      if(dictKey in firstDict) commonElem[dictKey] = firstDict[dictKey]
-    }
-  }
-  return Object.entries(commonElem).reduce((acc, elem) => {
-    acc + (Number(elem[0]) * elem[1])
-  }, 0)
+  return dictionaries
 }
 
 console.log(common( [1,2,2,3],[5,3,2,2],[7,3,2,2]))
