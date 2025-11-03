2b166302de05b2b6f04fad5b6d8d7603467e0f3f
VanoKi <kirsvano@gmail.com>
2025-11-03 21:03:57 +0300

    fix: solution (2025-11-03 21:03:56)

--- Dominant-array-elements/solution.js
+++ Dominant-array-elements/solution.js
@@ -1,14 +1,16 @@
 
 function nameFunction (params) {
-  const ans = [params.at(-1)]
+  const ans = []
   let maxElem = params.at(-1)
   for (let i = params.length - 2; i >= 0 ; i--) {
     if (params[i] > maxElem) {
+      console.log(params[i], maxElem)
       ans.unshift(maxElem)
       maxElem = params[i]
-
+      console.log(maxElem)
     }
   }
+  ans.unshift(maxElem)
   return ans
 }
 console.log(nameFunction([16,17,14,3,14,5,2]))
