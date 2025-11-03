bf1322031cef12a0aa19df60a81021af15fb0f51
VanoKi <kirsvano@gmail.com>
2025-11-03 19:37:38 +0300

    fix: loop (2025-11-03 19:37:38)

--- Dominant-array-elements/solution.js
+++ Dominant-array-elements/solution.js
@@ -1,8 +1,12 @@
 
 function nameFunction (params) {
-  for (let i = params.length - 1; i >= 0; i--) {
-    console.log(i, params[i])
+  const ans = []
+  for (let i = 0; i < params.length - 1; i++) {
+    if (params[i] > params[i + 1]) ans.push(params[i])
   }
+  ans.push(params.at(-1))
+  return ans
 }
 console.log(nameFunction([1,21,4,7,5]))
+console.log(nameFunction([5,4,3,2,1]))
 
