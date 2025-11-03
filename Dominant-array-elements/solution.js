2e90d1bcaaa9504e264f04821e2507241daaee22
VanoKi <kirsvano@gmail.com>
2025-11-03 20:21:54 +0300

    fix: change loop (2025-11-03 20:21:54)

--- Dominant-array-elements/solution.js
+++ Dominant-array-elements/solution.js
@@ -1,12 +1,17 @@
 
 function nameFunction (params) {
-  const ans = []
-  for (let i = 0; i < params.length - 1; i++) {
-    if (params[i] > params[i + 1]) ans.push(params[i])
+  const ans = [params.at(-1)]
+  let maxElem = params.at(-1)
+  for (let i = params.length - 2; i >= 0 ; i--) {
+    if (params[i] > maxElem) {
+      ans.unshift(maxElem)
+      maxElem = params[i]
+
+    }
   }
-  ans.push(params.at(-1))
   return ans
 }
-console.log(nameFunction([1,21,4,7,5]))
-console.log(nameFunction([5,4,3,2,1]))
+console.log(nameFunction([16,17,14,3,14,5,2]))
+    // [17,14,5,2]
+// console.log(nameFunction([5,4,3,2,1]))
 
