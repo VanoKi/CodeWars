63fbb33d4a0a59e6b4e04b34d5c4d02c43a1c473
VanoKi <kirsvano@gmail.com>
2025-11-03 17:06:17 +0300

    feat: second idea (2025-11-03 17:06:17)

--- Dominant-array-elements-js-7/solution.js
+++ Dominant-array-elements-js-7/solution.js
@@ -1,6 +1,18 @@
 
 function nameFunction (params) {
-  return params
+  const positive = []
+  const negative = []
+  for (const param of params.sort((a, b) => a - b)) {
+    if (param > 0) positive.push(param)
+    else negative.push(param)
+  }
+  // return [negative, positive]
+  // return params.reduce((sum, e) => sum + e)
+  for (const param of params) {
+    if(params.includes(-(param))) continue
+    else return param
+  }
 }
-console.log(nameFunction())
+console.log(nameFunction([1, -1, 2, -2, 3, 3]))
+console.log(nameFunction([-3, 1, 2, 3, -1, -4, -2]))
 
