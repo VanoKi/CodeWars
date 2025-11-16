84436ebcbf9aedea262b26e007b03ebed1c5d91b
VanoKi <kirsvano@gmail.com>
2025-11-16 19:43:02 +0300

    feat: dict (2025-11-16 19:43:02)

--- find-duplicates-js-7/solution.js
+++ find-duplicates-js-7/solution.js
@@ -1,6 +1,11 @@
 
 function nameFunction (params) {
-  return params
+  const dict = {}
+  for (const p of params) {
+    if(!dict[p]) dict[p] = 1
+    else dict[p] += 1
+  }
+  return dict
 }
 console.log(nameFunction([1, 2, 4, 4, 3, 3, 1, 5, 3, "5"]))
 
