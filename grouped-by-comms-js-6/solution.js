aaafca291380cc4c436a16b3f9ad0dd7302e4ae4
VanoKi <kirsvano@gmail.com>
2025-11-16 15:47:32 +0300

    feat: try to use reverse array (2025-11-16 15:47:32)

--- grouped-by-comms-js-6/solution.js
+++ grouped-by-comms-js-6/solution.js
@@ -1,12 +1,15 @@
 
 function nameFunction (params) {
+  // let str =  Array.from(String(params))
   let str =  String(params)
-  // for (let i = str.length - 1; i >= 0; i -= 3) {
-  //   console.log(str.slice(i, (i - 3)))
-  // }
-  for (let i = 0; i < str.length; i += 3) {
-    console.log(str.slice(i, i + 3))
+  for (let i = str.length ; i > -1; i -= 3) {
+    // console.log(str[i])
+    // console.log(i)
+    console.log(str.slice(i - 3, i))
   }
+  // for (let i = 0; i < str.length; i += 3) {
+  //   console.log(str.slice(i, i + 3))
+  // }
 }
 console.log(nameFunction(12345678))
 
