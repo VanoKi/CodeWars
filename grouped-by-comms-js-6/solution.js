4deedb29e9176fe29afbb510a6f7951183939702
VanoKi <kirsvano@gmail.com>
2025-11-16 15:31:18 +0300

    feat: two loops (2025-11-16 15:31:18)

--- grouped-by-comms-js-6/solution.js
+++ grouped-by-comms-js-6/solution.js
@@ -1,9 +1,12 @@
 
 function nameFunction (params) {
   let str =  String(params)
-  for (let i = 0; i < str.length; i++) {
-    
+  // for (let i = str.length - 1; i >= 0; i -= 3) {
+  //   console.log(str.slice(i, (i - 3)))
+  // }
+  for (let i = 0; i < str.length; i += 3) {
+    console.log(str.slice(i, i + 3))
   }
 }
-console.log(nameFunction(35235235))
+console.log(nameFunction(12345678))
 
