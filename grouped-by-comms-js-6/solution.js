ee407019fa069132bed7b9d2d2bc17fd600e158a
VanoKi <kirsvano@gmail.com>
2025-11-16 16:02:21 +0300

    feat: use reverse (2025-11-16 16:02:21)

--- grouped-by-comms-js-6/solution.js
+++ grouped-by-comms-js-6/solution.js
@@ -1,30 +1,11 @@
 
 function nameFunction (params) {
-  // let str =  Array.from(String(params))
-  let str =  String(params)
-  let count = 0
-  let word = []
-  let ans = []
-  for (let i = str.length ; i > -1; i -= 1) {
-    // console.log(str[i])
-    if (count !== 3) {
-      word.unshift(str[i])
-      count += 1
-      console.log(count)
-      console.log(word)
-    } else {
-      count = 0
-      ans.unshift(word)
-      word = []
-    }
-    // console.log(i)
-    // console.log(str.slice(i - 3, i))
+  let str =  Array.from(String(params)).reverse()
+  const ans = []
+  for (let i = 0; i < str.length; i += 3) {
+    ans.unshift(str.slice(i, i + 3).reverse().join(''))
   }
-  ans.unshift(word)
-  // for (let i = 0; i < str.length; i += 3) {
-  //   console.log(str.slice(i, i + 3))
-  // }
-  return ans
+  return ans.join(',')
 }
 console.log(nameFunction(12345678))
 
