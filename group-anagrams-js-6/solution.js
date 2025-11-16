6938f9efbefd55170b865a769047789c60b4f049
VanoKi <kirsvano@gmail.com>
2025-11-16 16:30:50 +0300

    feat: dictinary

--- group-anagrams-js-6/solution.js
+++ group-anagrams-js-6/solution.js
@@ -1,9 +1,13 @@
 
 function nameFunction (params) {
   let ans = {}
+  const getKey = (param) => param.split('').sort((a, b) => b < a).join('')
   for (const param of params) {
-    console.log(param, param.split('').sort((a, b) => b < a))
+    let key = getKey(param)
+    if (key in ans) ans[key].push(param)
+    else ans[key] = [param]
   }
+  return ans
 }
 console.log(nameFunction(["tsar", "rat", "tar", "star", "tars", "cheese"]
 ))
