57348867091bc2f87d46f27d07bbbe72995ffca3
VanoKi <kirsvano@gmail.com>
2025-11-16 19:55:07 +0300

    feat: dupl and stack (2025-11-16 19:55:07)

--- find-duplicates-js-7/solution.js
+++ find-duplicates-js-7/solution.js
@@ -1,11 +1,16 @@
 
 function nameFunction (params) {
-  const dict = {}
+  const dupl = []
+  const stack = []
   for (const p of params) {
-    if(!dict[p]) dict[p] = 1
-    else dict[p] += 1
+    console.log(typeof p)
+    if (typeof p === "number") {
+      console.log(p)
+      if (stack.includes(p)) dupl.push(p)
+      else stack.includes(p)
+    }
   }
-  return dict
+  return dupl
 }
 console.log(nameFunction([1, 2, 4, 4, 3, 3, 1, 5, 3, "5"]))
 
