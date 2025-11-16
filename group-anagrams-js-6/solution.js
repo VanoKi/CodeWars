821904279b867f784a0e8afdac922fbd55a58a2c
VanoKi <kirsvano@gmail.com>
2025-11-16 16:39:04 +0300

    fix: sort (2025-11-16 16:39:04)

--- group-anagrams-js-6/solution.js
+++ group-anagrams-js-6/solution.js
@@ -1,13 +1,13 @@
 
 function nameFunction (params) {
   let ans = {}
-  const getKey = (param) => param.split('').sort((a, b) => b < a).join('')
+  const getKey = (param) => param.split('').sort().join('')
   for (const param of params) {
     let key = getKey(param)
     if (key in ans) ans[key].push(param)
     else ans[key] = [param]
   }
-  return ans
+  return Object.values(ans)
 }
 console.log(nameFunction(["tsar", "rat", "tar", "star", "tars", "cheese"]
 ))
