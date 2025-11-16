619683a09072ff756eefc3b59e536e4d66f77ac7
VanoKi <kirsvano@gmail.com>
2025-11-16 16:21:02 +0300

    feat: add sort (2025-11-16 16:21:01)

--- group-anagrams-js-6/solution.js
+++ group-anagrams-js-6/solution.js
@@ -1,6 +1,9 @@
 
 function nameFunction (params) {
-  return params
+  let ans = {}
+  for (const param of params) {
+    console.log(param, param.split('').sort((a, b) => b < a))
+  }
 }
 console.log(nameFunction(["tsar", "rat", "tar", "star", "tars", "cheese"]
 ))
