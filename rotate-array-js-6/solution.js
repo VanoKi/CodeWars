7d96e3951798a51c8204f7732440eb0cd6cc0e06
VanoKi <kirsvano@gmail.com>
2025-11-17 18:49:09 +0300

    fix: del comments (2025-11-17 18:49:09)

--- rotate-array-js-6/solution.js
+++ rotate-array-js-6/solution.js
@@ -1,11 +1,7 @@
 
 function nameFunction (params, number) {
   let n = number % params.length
-  // if (n < 0){
-  //   return params.slice(-n).concat(params.slice(0, -n))
-  // }
-  console.log(n)
-  return params.slice(n).concat(params.slice(0, n))
+  return params.slice(-n).concat(params.slice(0, -n))
 }
-console.log(nameFunction([1, 2, 3, 4, 5,], 4))
+console.log(nameFunction([1, 2, 3, 4, 5,], 12478))
 
