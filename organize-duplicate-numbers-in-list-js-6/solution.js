66a3ad09fd7edfd4a6927d20e5533623cd478309
VanoKi <kirsvano@gmail.com>
2025-11-17 19:14:28 +0300

    fix: return to array (2025-11-17 19:14:28)

--- organize-duplicate-numbers-in-list-js-6/solution.js
+++ organize-duplicate-numbers-in-list-js-6/solution.js
@@ -1,13 +1,15 @@
 
 function nameFunction (params) {
   const ans = []
-  const count = []
   for (const param of params) {
-    if (count.includes(param)) ans.push([param])
-    else count.push(param)
+    if (ans.includes(ans[param])){
+      console.log(ans.includes(ans[param]))
+      console.log(ans.indexOf(ans[param]))
+      ans[param].push(param)
+    }
+    else ans.push([param])
   }
-  // return ans
-  return params.map(e => [e])
+  return ans
 }
 console.log(nameFunction([3, 2, 6, 2, 1, 3]))
 
