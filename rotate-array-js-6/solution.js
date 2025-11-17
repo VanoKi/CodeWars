d5aff468191e1b7ed05f76cd3e3a41754ab1cbd3
VanoKi <kirsvano@gmail.com>
2025-11-17 18:41:33 +0300

    fix: typo (2025-11-17 18:41:33)

--- rotate-array-js-6/solution.js
+++ rotate-array-js-6/solution.js
@@ -1,6 +1,11 @@
 
-function nameFunction (params, n) {
+function nameFunction (params, number) {
+  let n = number % params.length
+  // if (n < 0){
+  //   return params.slice(-n).concat(params.slice(0, -n))
+  // }
+  console.log(n)
   return params.slice(n).concat(params.slice(0, n))
 }
-console.log(nameFunction([1, 2, 3, 4, 5,], 1))
+console.log(nameFunction([1, 2, 3, 4, 5,], 4))
 
