c84ff9ff5f098bbc89ed8c35301c1054d344f4a0
VanoKi <kirsvano@gmail.com>
2025-11-17 15:46:21 +0300

    fix: dict and reduce (2025-11-17 15:46:20)

--- number-of-anagrams-in-an-array-of-words-js-6/solution.js
+++ number-of-anagrams-in-an-array-of-words-js-6/solution.js
@@ -5,13 +5,13 @@ function nameFunction (params) {
   for (const param of params) {
     let keyWord = key(param)
     if (dict[keyWord]) dict[keyWord].push(param)
-    else dict[keyWord] = [keyWord]
+    else dict[keyWord] = [param]
   }
   const countCouples = (arr) => {
     let arrLength = arr.length
     return arrLength * ( arrLength - 1) / 2
   }
-  return Object.values(dict).map(countCouples).reduce((sum, arr) => sum + arr)
+  return Object.values(dict).map(countCouples).reduce((sum, arr) => sum + arr, 0)
 }
 console.log(nameFunction(["dell", "ledl", "abc", "cba", "bca", "bac"]))
 
