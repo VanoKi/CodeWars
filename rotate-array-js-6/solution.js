3dbedcb942dd0c1e4048ef9aedfc88cda2749e03
VanoKi <kirsvano@gmail.com>
2025-11-17 16:21:17 +0300

    feat: add slice (2025-11-17 16:21:17)

--- rotate-array-js-6/solution.js
+++ rotate-array-js-6/solution.js
@@ -1,6 +1,6 @@
 
-function nameFunction (params) {
-  return params
+function nameFunction (params, n) {
+  return params.slice(n).concat(params.slice(0, n))
 }
-console.log(nameFunction([1, 2, 3, 4, 5]))
+console.log(nameFunction([1, 2, 3, 4, 5,], 1))
 
