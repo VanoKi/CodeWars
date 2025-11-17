ebaaa2cfa5d3790a7841690878799828f6bdad40
VanoKi <kirsvano@gmail.com>
2025-11-17 18:58:40 +0300

    feat: add dict (2025-11-17 18:58:40)

--- organize-duplicate-numbers-in-list-js-6/solution.js
+++ organize-duplicate-numbers-in-list-js-6/solution.js
@@ -1,6 +1,11 @@
 
 function nameFunction (params) {
-  return params
+  const dict = {}
+  for (const param of params) {
+    if (dict[param]) dict[param].push(param)
+    else dict[param] = [param]
+  }
+  return Object.values(dict)
 }
 console.log(nameFunction([3, 2, 6, 2, 1, 3]))
 
