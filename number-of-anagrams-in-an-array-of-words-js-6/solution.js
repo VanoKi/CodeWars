a5c324aa0ecd35286548b401a6b74806c0344a22
VanoKi <kirsvano@gmail.com>
2025-11-17 15:29:19 +0300

    fix: keyWord (2025-11-17 15:29:19)

--- number-of-anagrams-in-an-array-of-words-js-6/solution.js
+++ number-of-anagrams-in-an-array-of-words-js-6/solution.js
@@ -1,6 +1,14 @@
 
 function nameFunction (params) {
-  return params
+  const key = (param) => param.split('').sort().join('')
+  const dict = {}
+  for (const param of params) {
+    let keyWord = key(param)
+    // console.log(keyWord)
+    if (dict[keyWord]) dict[keyWord].push(param)
+    else dict[keyWord] = [keyWord]
+  }
+  return key(params[0])
 }
-console.log(nameFunction())
+console.log(nameFunction(["dell", "ledl", "abc", "cba", "bca", "bac"]))
 
