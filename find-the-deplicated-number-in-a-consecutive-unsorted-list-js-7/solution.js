b9aadccd9a7f161ea08ae74549af436d457a44f4
VanoKi <kirsvano@gmail.com>
2025-11-18 13:09:16 +0300

    feat: solution (2025-11-18 13:09:16)

--- find-the-deplicated-number-in-a-consecutive-unsorted-list-js-7/solution.js
+++ find-the-deplicated-number-in-a-consecutive-unsorted-list-js-7/solution.js
@@ -1,6 +1,10 @@
 
 function nameFunction (params) {
-  return params
+  const seem = []
+  for (const param of params) {
+    if (seem.includes(param)) return param
+    else seem.push(param)
+  }
 }
 console.log(nameFunction([3, 2, 5, 1, 3, 4]))
 
