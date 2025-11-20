1309337cbe5f9f317079677477a1dcd8cda1283f
VanoKi <kirsvano@gmail.com>
2025-11-20 17:42:35 +0300

    feat: add loop (2025-11-20 17:42:35)

--- alternate-capitalization-ts-7/solution.ts
+++ alternate-capitalization-ts-7/solution.ts
@@ -1,3 +1,16 @@
 export function capitalize(s: string): [string, string] {
-  return ["", ""];
+  let odd = ''
+  let even = ''
+  for (let i = 0; i < s.length; i++) {
+    if (i % 2 == 1) {
+      even += s[i].toUpperCase()
+      odd += s[i]
+    } else {
+      even += s[i]
+      odd += s[i].toUpperCase()
+    }
+  }
+  return [odd, even];
 }
+
+console.log(capitalize("abcdef"))
