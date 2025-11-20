070572e3807290598e0e14ea1cc42471effea4a2
VanoKi <kirsvano@gmail.com>
2025-11-20 16:26:56 +0300

    feat: add minElem (2025-11-20 16:26:56)

--- your-order-please-js-6/solution.js
+++ your-order-please-js-6/solution.js
@@ -1,10 +1,20 @@
 
 function nameFunction (params) {
   const numbers ='0123456789'
+  let minElem = 0
+  let ans = []
   for (const param of params.split(' ')) {
     console.log(param)
+    for (const paramElement of param) {
+      if (numbers.includes(paramElement)) {
+        if (minElem < Number(paramElement)) {
+          ans.push(param)
+          minElem = Number(paramElement)
+        }
+      }
+    }
   }
-  return params
+  return ans
 }
 console.log(nameFunction("is2 Thi1s T4est 3a"))
 
