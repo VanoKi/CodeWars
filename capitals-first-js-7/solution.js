13926d171e17e59eee0cc27166d1d0f56b97eeaf
VanoKi <kirsvano@gmail.com>
2025-11-20 17:17:18 +0300

    fix: add second if (2025-11-20 17:17:18)

--- capitals-first-js-7/solution.js
+++ capitals-first-js-7/solution.js
@@ -3,12 +3,18 @@ function nameFunction (params) {
   const capitals = []
   const low = []
   for (const p of params.split(' ')) {
-    let firstChar = p[0].charCodeAt(0)
-    if (firstChar >= 65 && firstChar <= 90) capitals.push(p)
-    else low.push(p)
+    let char = p[0].toUpperCase().charCodeAt(0)
+    if (!(char >64 && char < 91)) continue
+    else {
+      let firstChar = p[0].charCodeAt(0)
+      if (firstChar >= 65 && firstChar <= 90) capitals.push(p)
+      else low.push(p)
+    }
   }
   return capitals.concat(low).join(' ')
 }
-console.log(nameFunction("hey You, Sort me Already!"))
+console.log(nameFunction('123 baby You and Me'))
+// console.log('a'.charCodeAt(0))
+// console.log('z'.charCodeAt(0))
 // console.log('A'.charCodeAt(0))
 // console.log('Z'.charCodeAt(0))
