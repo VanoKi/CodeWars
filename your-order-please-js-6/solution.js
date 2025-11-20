2d66f674d6d4648c53c5918b132f05b1b3310f70
VanoKi <kirsvano@gmail.com>
2025-11-20 16:23:10 +0300

    feat: first loop (2025-11-20 16:23:09)

---
+++ your-order-please-js-6/solution.js
@@ -0,0 +1,10 @@
+
+function nameFunction (params) {
+  const numbers ='0123456789'
+  for (const param of params.split(' ')) {
+    console.log(param)
+  }
+  return params
+}
+console.log(nameFunction("is2 Thi1s T4est 3a"))
+
