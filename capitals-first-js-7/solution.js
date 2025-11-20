893f7507cb80e8c134719ddfdf1cde5f04288861
VanoKi <kirsvano@gmail.com>
2025-11-20 17:10:32 +0300

    fix: if (2025-11-20 17:10:32)

--- capitals-first-js-7/solution.js
+++ capitals-first-js-7/solution.js
@@ -3,10 +3,12 @@ function nameFunction (params) {
   const capitals = []
   const low = []
   for (const p of params.split(' ')) {
-    if (p[0].toUpperCase()) capitals.push(p)
+    let firstChar = p[0].charCodeAt(0)
+    if (firstChar >= 65 && firstChar <= 90) capitals.push(p)
     else low.push(p)
   }
   return capitals.concat(low).join(' ')
 }
 console.log(nameFunction("hey You, Sort me Already!"))
-
+// console.log('A'.charCodeAt(0))
+// console.log('Z'.charCodeAt(0))
