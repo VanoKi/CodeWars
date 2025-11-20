01a90f3c3bf60d955440eb2d919cd230e4c16e09
VanoKi <kirsvano@gmail.com>
2025-11-20 16:57:17 +0300

    feat: first solution (2025-11-20 16:57:17)

--- capitals-first-js-7/solution.js
+++ capitals-first-js-7/solution.js
@@ -1,6 +1,12 @@
 
 function nameFunction (params) {
-  return params
+  const capitals = []
+  const low = []
+  for (const p of params.split(' ')) {
+    if (p[0].toUpperCase()) capitals.push(p)
+    else low.push(p)
+  }
+  return capitals.concat(low).join(' ')
 }
-console.log(nameFunction())
+console.log(nameFunction("hey You, Sort me Already!"))
 
