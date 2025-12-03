f4ece87f12d1f3366e1c78c07da2108984c8bd1c
VanoKi <kirsvano@gmail.com>
2025-12-03 21:25:06 +0300

    fix: string (2025-12-03 21:25:06)

--- String-Scramble-js-7/solution.js
+++ String-Scramble-js-7/solution.js
@@ -3,7 +3,7 @@ function scramble(str, arr) {
   for (let i = 0; i < str.length; i++) {
     ans[arr[i]] = str[i]
   }
-  return ans
+  return ans.join('')
 };
 
 console.log(scramble("abcd", [0, 3, 1, 2]))
