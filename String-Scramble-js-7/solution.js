d4e876fcb2663dd797910880efbae6f4f8ddd6bc
VanoKi <kirsvano@gmail.com>
2025-12-03 21:24:29 +0300

    feat: first solution (2025-12-03 21:24:29)

--- String-Scramble-js-7/solution.js
+++ String-Scramble-js-7/solution.js
@@ -1,6 +1,9 @@
+function scramble(str, arr) {
+  const ans = []
+  for (let i = 0; i < str.length; i++) {
+    ans[arr[i]] = str[i]
+  }
+  return ans
+};
 
-function nameFunction (params) {
-  return params
-}
-console.log(nameFunction())
-
+console.log(scramble("abcd", [0, 3, 1, 2]))
