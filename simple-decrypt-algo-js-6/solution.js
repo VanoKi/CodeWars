b670c2644dbaad33e94c6a6f21d1716ca178fda2
VanoKi <kirsvano@gmail.com>
2025-12-04 15:04:09 +0300

    feat: dictinary (2025-12-04 15:04:09)

--- simple-decrypt-algo-js-6/solution.js
+++ simple-decrypt-algo-js-6/solution.js
@@ -1,6 +1,16 @@
 
 function nameFunction (params) {
-  return params
-}
-console.log(nameFunction())
 
+  const createDict = () => {
+    const dict = {}
+    for (let i = 97; i < 123; i++) {
+      console.log(String.fromCharCode(i))
+    }
+    return dict
+  }
+  return createDict()
+}
+console.log(nameFunction('$aaaa#bbb*cc^fff!z\''))
+
+console.log('a'.charCodeAt(0))
+console.log('z'.charCodeAt(0))
