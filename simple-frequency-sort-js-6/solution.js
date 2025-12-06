4500aa01990bb4bb382e7647e31f46cbc55ef625
VanoKi <kirsvano@gmail.com>
2025-12-06 18:30:48 +0300

    feat: sort dict (2025-12-06 18:30:48)

--- simple-frequency-sort-js-6/solution.js
+++ simple-frequency-sort-js-6/solution.js
@@ -5,7 +5,7 @@ function nameFunction (params) {
     if (dict[elem]) dict[elem] += 1
     else dict[elem] = 1
   }
-  return dict
+  return Object.entries(dict).sort( (a, b) => a[1] < b[1])
 }
 console.log(nameFunction([2,3,5,3,7,9,5,3,7]))
 
