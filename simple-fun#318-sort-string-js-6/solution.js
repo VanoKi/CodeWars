2dc845d87b488f55d7dbece4a277aef39f4e26ae
VanoKi <kirsvano@gmail.com>
2025-12-06 19:57:52 +0300

    fix: loops (2025-12-06 19:57:51)

--- simple-fun#318-sort-string-js-6/solution.js
+++ simple-fun#318-sort-string-js-6/solution.js
@@ -1,12 +1,8 @@
-
-function nameFunction (params) {
+function nameFunction(params) {
   let abc = ''
-  for (let i = 97; i < 123; i++) {
-    abc += String.fromCharCode(i)
-  }
-  for (let i = 65; i < 91; i++) {
-    abc += String.fromCharCode(i)
-  }
+  for (let i = 97; i < 123; i++) abc += String.fromCharCode(i)
+  for (let i = 65; i < 91; i++) abc += String.fromCharCode(i)
+
   const ans = []
   const toSort = []
   for (const elem of params) {
@@ -28,12 +24,12 @@ function nameFunction (params) {
       ans[i] = sorted.shift()
     }
   }
-  return ans
+  return ans.join('')
 }
+
 console.log(nameFunction("cCBbAa"))
 
 console.log('a'.charCodeAt(0))
 console.log('z'.charCodeAt(0))
 console.log('A'.charCodeAt(0))
 console.log('Z'.charCodeAt(0))
-
