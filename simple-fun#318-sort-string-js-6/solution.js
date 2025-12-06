b5becdd71e5ffea77fd270d5f0f6a151ab175c9e
VanoKi <kirsvano@gmail.com>
2025-12-06 19:41:06 +0300

    feat: add second loop (2025-12-06 19:41:05)

--- simple-fun#318-sort-string-js-6/solution.js
+++ simple-fun#318-sort-string-js-6/solution.js
@@ -1,8 +1,25 @@
 
 function nameFunction (params) {
-  return Array.from(params).sort()
+  let abc = ''
+  for (let i = 97; i < 123; i++) {
+    abc += String.fromCharCode(i)
+  }
+  for (let i = 65; i < 91; i++) {
+    abc += String.fromCharCode(i)
+  }
+  // return abc
+  const ans = []
+  const toSort = []
+  for (const elem of params) {
+    if (!abc.includes(elem)) ans.push(elem)
+    else {
+      ans.push('')
+      toSort.push(elem)
+    }
+  }
+  return toSort.sort()
 }
-console.log(nameFunction("-a--b--c-"))
+console.log(nameFunction("cCBbAa"))
 
 console.log('a'.charCodeAt(0))
 console.log('z'.charCodeAt(0))
