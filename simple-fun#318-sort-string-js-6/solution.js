4bb0dd1af06329d9d1a21de8edc45817642e6173
VanoKi <kirsvano@gmail.com>
2025-12-06 19:19:08 +0300

    feat: add charCodeAt (2025-12-06 19:19:07)

--- simple-fun#318-sort-string-js-6/solution.js
+++ simple-fun#318-sort-string-js-6/solution.js
@@ -1,6 +1,11 @@
 
 function nameFunction (params) {
-  return params
+  return Array.from(params).sort()
 }
-console.log(nameFunction())
+console.log(nameFunction("-a--b--c-"))
+
+console.log('a'.charCodeAt(0))
+console.log('z'.charCodeAt(0))
+console.log('A'.charCodeAt(0))
+console.log('Z'.charCodeAt(0))
 
