ca9f0edb5c6f9e33687301ee860ced8ef8800d5d
VanoKi <kirsvano@gmail.com>
2025-12-06 18:00:49 +0300

    feat: creat dictionary (2025-12-06 18:00:49)

--- simple-frequency-sort-js-6/solution.js
+++ simple-frequency-sort-js-6/solution.js
@@ -1,6 +1,11 @@
 
 function nameFunction (params) {
-  return params
+  const dict = {}
+  for (const elem of params) {
+    if (dict[elem]) dict[elem] += 1
+    else dict[elem] = 1
+  }
+  return dict
 }
 console.log(nameFunction([2,3,5,3,7,9,5,3,7]))
 
