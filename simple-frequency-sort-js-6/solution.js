1dade34d94d5f22b23a6aace79cd2dac3eb648be
VanoKi <kirsvano@gmail.com>
2025-12-06 18:40:58 +0300

    feat: createArray (2025-12-06 18:40:58)

--- simple-frequency-sort-js-6/solution.js
+++ simple-frequency-sort-js-6/solution.js
@@ -5,7 +5,15 @@ function nameFunction (params) {
     if (dict[elem]) dict[elem] += 1
     else dict[elem] = 1
   }
-  return Object.entries(dict).sort( (a, b) => a[1] < b[1])
+  const createArray = (a, b) => {
+    const ans = []
+    for (let i = 0; i < b ; i++) {
+      ans.push(+a)
+    }
+    return ans
+  }
+  // return Object.entries(dict).sort( (a, b) =>  b[1] - a[1]).map(e => +e[0])
+  return createArray('3', 3)
 }
 console.log(nameFunction([2,3,5,3,7,9,5,3,7]))
 
