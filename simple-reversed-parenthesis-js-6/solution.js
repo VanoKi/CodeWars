f0720c8d7e267a0b197bf9f68002067e54aa7db9
VanoKi <kirsvano@gmail.com>
2025-12-08 19:59:17 +0300

    feat: add if (2025-12-08 19:59:17)

--- simple-reversed-parenthesis-js-6/solution.js
+++ simple-reversed-parenthesis-js-6/solution.js
@@ -11,7 +11,10 @@ function nameFunction (params) {
       }
     }
   }
-  return stack
+  if (stack.length % 2 !== 0) return -1
+  const opens = stack.filter(c => c === '(').length
+  const closes = stack.length - opens
+  return Math.ceil(opens / 2) + Math.ceil(closes / 2)
 }
 console.log(nameFunction("(((())"))
 
