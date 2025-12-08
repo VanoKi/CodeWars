0ed0d3246d9cc29186e83d38dc9cac8752bbdfee
VanoKi <kirsvano@gmail.com>
2025-12-08 17:15:58 +0300

    feat: create stack (2025-12-08 17:15:58)

--- simple-reversed-parenthesis-js-6/solution.js
+++ simple-reversed-parenthesis-js-6/solution.js
@@ -1,6 +1,11 @@
 
 function nameFunction (params) {
-  return params
+  const stack = []
+  for (let i = 0; i < params.length; i++) {
+    if (params[i] == '(') stack.push(params[i])
+    else stack.pop()
+  }
+  return stack
 }
 console.log(nameFunction("(((())"))
 
