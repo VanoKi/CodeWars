4ed97c55129d50d06963eea5fc2e583ded8b049d
VanoKi <kirsvano@gmail.com>
2025-12-08 20:28:03 +0300

    feat: create lower (2025-12-08 20:28:03)

--- sentence-calculator-js-6/solution.js
+++ sentence-calculator-js-6/solution.js
@@ -1,6 +1,9 @@
 
 function nameFunction (params) {
-  return params
+  let lower = ''
+  for (let i = 97; i < 123; i++) {
+    lower += String.fromCharCode(i)
+  }
 }
-console.log(nameFunction())
+console.log(nameFunction("I Love You"))
 
