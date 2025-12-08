0f4b63d21d6b4c8c19552ea8eb679b9a1546b2bb
VanoKi <kirsvano@gmail.com>
2025-12-08 20:38:12 +0300

    feat: add upper (2025-12-08 20:38:12)

--- sentence-calculator-js-6/solution.js
+++ sentence-calculator-js-6/solution.js
@@ -2,11 +2,15 @@
 function nameFunction (params) {
   const lower = {}
   let count = 1
+  const upper = {}
+  let upperCount = 2
   for (let i = 97; i < 123; i++) {
     lower[String.fromCharCode(i)] = count
     count += 1
+    upper[String.fromCharCode(i).toUpperCase()] = upperCount
+    upperCount += 2
   }
-  return lower
+  return upper
 }
 console.log(nameFunction("I Love You"))
 console.log('A'.charCodeAt(0))
