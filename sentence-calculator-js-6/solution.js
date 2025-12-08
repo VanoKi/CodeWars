626c290a815d3e3431862c7b4bbc5d3f4b197f32
VanoKi <kirsvano@gmail.com>
2025-12-08 20:35:34 +0300

    fix: lower (2025-12-08 20:35:34)

--- sentence-calculator-js-6/solution.js
+++ sentence-calculator-js-6/solution.js
@@ -1,9 +1,16 @@
 
 function nameFunction (params) {
-  let lower = ''
+  const lower = {}
+  let count = 1
   for (let i = 97; i < 123; i++) {
-    lower += String.fromCharCode(i)
+    lower[String.fromCharCode(i)] = count
+    count += 1
   }
+  return lower
 }
 console.log(nameFunction("I Love You"))
+console.log('A'.charCodeAt(0))
+console.log('Z'.charCodeAt(0))
+console.log('0'.charCodeAt(0))
+console.log('9'.charCodeAt(0))
 
