cc1f2feb93a4405b56f5f5bc358b445f35ea5a70
VanoKi <kirsvano@gmail.com>
2025-12-08 19:48:38 +0300

    fix: stack (2025-12-08 19:48:38)

--- simple-reversed-parenthesis-js-6/solution.js
+++ simple-reversed-parenthesis-js-6/solution.js
@@ -3,7 +3,13 @@ function nameFunction (params) {
   const stack = []
   for (let i = 0; i < params.length; i++) {
     if (params[i] == '(') stack.push(params[i])
-    else stack.pop()
+    else {
+      if (stack.length >0 && stack[stack.length -1] === '(') {
+        stack.pop()
+      } else {
+        stack.push(')')
+      }
+    }
   }
   return stack
 }
