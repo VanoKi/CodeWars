a1474dfc26fc04138810b752a67f778876f82020
VanoKi <kirsvano@gmail.com>
2026-03-14 14:51:00 +0300

    feat: reverse loop

--- Validate-credit-card-number--js-6/solution.js
+++ Validate-credit-card-number--js-6/solution.js
@@ -1,6 +1,9 @@
-
-function nameFunction (params) {
-  return params
+function nameFunction(params) {
+  const arr = Array.from('' + params);
+  const sum = arr[arr.length - 1];
+  for (let i = arr.length - 1; i >= 0; i--) {
+    console.log(arr[i]);
+  }
+  return sum;
 }
-console.log(nameFunction())
-
+console.log(nameFunction(12345));
