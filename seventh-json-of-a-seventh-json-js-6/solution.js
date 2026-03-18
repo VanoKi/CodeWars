91cac328e6c8d38ab860b38469331c8d9e162265
VanoKi <kirsvano@gmail.com>
2026-03-18 10:18:50 +0300

    feat: start kata

---
+++ seventh-json-of-a-seventh-json-js-6/solution.js
@@ -0,0 +1,39 @@
+const param = {
+  name: 'A',
+  gender: 'male',
+  children: [
+    { name: 'BBBB', gender: 'female', children: [] },
+    { name: 'B', gender: 'male', children: [] },
+    { name: 'C', gender: 'male', children: [] },
+    { name: 'D', gender: 'male', children: [] },
+    { name: 'E', gender: 'male', children: [] },
+    { name: 'F', gender: 'male', children: [] },
+    { name: 'G', gender: 'male', children: [] },
+    {
+      name: 'H',
+      gender: 'male',
+      children: [
+        { name: 'I', gender: 'male', children: [] },
+        { name: 'J', gender: 'male', children: [] },
+        { name: 'K', gender: 'male', children: [] },
+        { name: 'L', gender: 'male', children: [] },
+        { name: 'M', gender: 'male', children: [] },
+        { name: 'N', gender: 'male', children: [] },
+        {
+          name: 'O',
+          gender: 'male',
+          children: [],
+        },
+      ],
+    },
+  ],
+};
+function nameFunction(params) {
+  const checkSeven = (person) => {
+    if (person.gender === 'male') {
+      console.log(person.name);
+    }
+  };
+  return checkSeven(param);
+}
+console.log(nameFunction(param));
